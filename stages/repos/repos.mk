7e18253751b1d980065566c8bdfd08ce89c6e3ff
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-04 18:23:10 +0100

    Managing debian repositories

--- stages/repos/repos.mk
+++ stages/repos/repos.mk
@@ -1,2 +1,5 @@
-repos:
-	@echo "Setting up Debian repositories"
+repos: setup_gpg_keys
+	@printf "`tput bold`Setting up Debian repositories completed`tput sgr0`\n"
+
+setup_gpg_keys:
+	jq -cr '.debian.repositories[].key' variables/main.json | xargs apt-key adv --fetch-keys --no-tty
