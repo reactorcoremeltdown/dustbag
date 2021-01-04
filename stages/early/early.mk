9ecedf7d9070ffc8e034bd7fea8eff5a16b68910
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-04 13:51:49 +0100

    Fixed directory path

--- stages/early/early.mk
+++ stages/early/early.mk
@@ -3,6 +3,6 @@ early:
 	@echo "Early stage"
 	jq --version
 	test $(UNAME) = "Linux" || exit 1
-	if ! test -f /etc/defaults/earlystageconfigs; then \
+	if ! test -f /etc/default/earlystageconfigs; then \
 		echo "provisioning done" > /etc/defaults/earlystageconfigs; \
 	fi
