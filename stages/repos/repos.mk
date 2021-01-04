0ff1ef257f5bb574f8b4b2d04775bbc67fcab014
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-04 18:35:37 +0100

    Dummy repo config

--- stages/repos/repos.mk
+++ stages/repos/repos.mk
@@ -1,5 +1,11 @@
-repos: setup_gpg_keys
+repos: setup_debian_repositories update_debian_repositories
 	@printf "`tput bold`Setting up Debian repositories completed`tput sgr0`\n"
 
 setup_gpg_keys:
 	jq -cr '.debian.repositories[].key' variables/main.json | xargs apt-key adv --fetch-keys --no-tty
+
+setup_debian_repositories: setup_gpg_keys
+	@echo "Not implemented yet"
+
+update_debian_repositories:
+	apt update
