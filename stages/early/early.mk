56258fd5b12141de296256bb4c40b42208dbfb3b
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-04 13:37:40 +0100

    Modified early stage

--- stages/early/early.mk
+++ stages/early/early.mk
@@ -1,3 +1,8 @@
+UNAME := $(shell uname)
 early:
 	@echo "Early stage"
 	jq --version
+	test $(UNAME) = "Linux" || exit 1
+	if ! test -f /etc/defaults/earlystageconfigs; then
+		echo "provisioning done" > /etc/defaults/earlystageconfigs
+	fi
