1637cefdee26e80cfce6686a8188e316511de379
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-04 14:01:57 +0100

    Fixed a wrong path

--- stages/early/early.mk
+++ stages/early/early.mk
@@ -4,5 +4,5 @@ early:
 	jq --version
 	test $(UNAME) = "Linux" || exit 1
 	if ! test -f /etc/default/earlystageconfigs; then \
-		echo "provisioning done" > /etc/defaults/earlystageconfigs; \
+		echo "provisioning done" > /etc/default/earlystageconfigs; \
 	fi
