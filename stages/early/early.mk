954efe184a62ddf14b901495b90d04c754462ba2
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-04 14:49:57 +0100

    Completing early config stage

--- stages/early/early.mk
+++ stages/early/early.mk
@@ -1,8 +1,21 @@
 UNAME := $(shell uname)
-early:
+HOSTNAME := "buster.rcmd.space"
+early: test shell_history hostname apt_configs
 	@echo "Early stage"
+	echo "provisioning done" > /etc/default/earlystageconfigs; \
+
+test:
 	jq --version
 	test $(UNAME) = "Linux" || exit 1
-	if ! test -f /etc/default/earlystageconfigs; then \
-		echo "provisioning done" > /etc/default/earlystageconfigs; \
-	fi
+
+shell_history:
+	ln -s /dev/null /root/.zsh_history
+	ln -s /dev/null /root/.bash_history
+
+hostname:
+	hostnamectl set-hostname $(HOSTNAME)
+
+apt_configs:
+	cp files/forceuserdefinedconfigs /etc/apt/apt.conf.d/forceuserdefinedconfigs
+	chmod 644 /etc/apt/apt.conf.d/forceuserdefinedconfigs
+	chown root:root /etc/apt/apt.conf.d/forceuserdefinedconfigs
