710f820ab6856c1bdaf1fe9d8b7e9ca0d3a99192
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-04 14:52:15 +0100

    Added keygen stage to early configs

--- stages/early/early.mk
+++ stages/early/early.mk
@@ -1,6 +1,6 @@
 UNAME := $(shell uname)
 HOSTNAME := "buster.rcmd.space"
-early: test shell_history hostname apt_configs
+early: test shell_history hostname apt_configs keygen
 	@echo "Early stage"
 	echo "provisioning done" > /etc/default/earlystageconfigs; \
 
@@ -19,3 +19,6 @@ apt_configs:
 	cp files/forceuserdefinedconfigs /etc/apt/apt.conf.d/forceuserdefinedconfigs
 	chmod 644 /etc/apt/apt.conf.d/forceuserdefinedconfigs
 	chown root:root /etc/apt/apt.conf.d/forceuserdefinedconfigs
+
+keygen:
+	test -f /root/.ssh/id_rsa || ssh-keygen -t rsa -N '' -f /root/.ssh/id_rsa
