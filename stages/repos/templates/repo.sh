068659a768d987145c7f0e1b5d0211a642f6a8bb
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-05 15:23:13 +0100

    Forgot a pipe

--- stages/repos/templates/repo.sh
+++ stages/repos/templates/repo.sh
@@ -7,7 +7,7 @@ for repo in `jq -c '.debian.repositories[]' ${1}`; do
     STATE=`echo ${repo} | jq -r '.state'`
     REPONAME=`echo ${repo} | jq -r '.name'`
     if [[ ${STATE} = 'present' ]]; then
-        echo "${repo}" jq -cr '. | "deb \(.url) DISTRO \(.section)"' | sed "s|DISTRO|${DISTRO}|g" > /etc/apt/sources.list.d/${REPONAME}.list
+        echo "${repo}" | jq -cr '. | "deb \(.url) DISTRO \(.section)"' | sed "s|DISTRO|${DISTRO}|g" > /etc/apt/sources.list.d/${REPONAME}.list
     else
         rm -f /etc/apt/sources.list.d/${REPONAME}.list || true
     fi
