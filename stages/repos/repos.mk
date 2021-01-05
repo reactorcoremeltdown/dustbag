8b5bade28e3bf2a26be297c2efd7cce89fbf7bc7
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-05 15:21:04 +0100

    Fixed a typo in the filename

--- stages/repos/repos.mk
+++ stages/repos/repos.mk
@@ -5,7 +5,7 @@ setup_gpg_keys:
 	jq -cr '.debian.repositories[].key' variables/main.json | xargs apt-key adv --fetch-keys --no-tty
 
 setup_debian_repositories: setup_gpg_keys
-	bash stages/repos/templates/repos.sh variables/main.json
+	bash stages/repos/templates/repo.sh variables/main.json
 
 update_debian_repositories:
 	apt update
