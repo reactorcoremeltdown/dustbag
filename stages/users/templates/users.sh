fff5e98e2df38ae620dd2e1a59640965c15c13cd
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-06 18:26:37 +0100

    Managing unix groups, initial stubs

--- stages/users/templates/users.sh
+++ stages/users/templates/users.sh
@@ -6,10 +6,12 @@ for user in `jq -cr '.users[]' ${1}`; do
     USERNAME=`echo "${user}" | jq -r '.name'`
     SHELL=`echo "${user}" | jq -r '.shell'`
     KEYGEN=`echo "${user}" | jq -r '.keygen'`
+    GROUPS=`echo "${user}" | jq -r '.groups | join(",")'`
     if ! groups ${USERNAME}; then
         useradd -s ${SHELL} ${USERNAME}
         if [[ ${KEYGEN} = 'true' ]]; then
             su ${USERNAME} -c 'ssh-keygen -b 2048 -t rsa q -N ""'
         fi
     fi
+    echo ${GROUPS}
 done
