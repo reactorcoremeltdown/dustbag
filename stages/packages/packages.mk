45f8d5737c87a74b4fc672348c82033f1f6d9018
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-06 09:48:59 +0100

    Formatting improvements

--- stages/packages/packages.mk
+++ stages/packages/packages.mk
@@ -1,5 +1,5 @@
 PACKAGES := $(shell jq -cr '.debian.packages[]' variables/main.json | xargs)
 
 packages: repos
-	dpkg-query -s $(PACKAGES) || apt-get install -y $(PACKAGES)
-	@printf "`tput bold`Installing Debian packages complete`tput sgr0`"
+	dpkg-query -s $(PACKAGES) > /dev/null || apt-get install -y $(PACKAGES)
+	@printf "`tput bold`Installing Debian packages complete`tput sgr0`\n"
