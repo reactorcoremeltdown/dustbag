c0bc48bfc226606cf8bca0ec33ab058840446c5f
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-12 17:34:17 +0100

    Changed suffix for monit config files

--- stages/monitoring/templates/checks.sh
+++ stages/monitoring/templates/checks.sh
@@ -6,7 +6,7 @@ for check in $(jq -cr '.checks_templates[]' ${1}); do
     source <(echo "${check}" | jq  -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     let "interval = interval * 60"
     echo "Check name: ${name}"
-    cat << EOF > /etc/monit/conf.d/${name}.conf
+    cat << EOF > /etc/monit/conf.d/${name}.cfg
 check program ${name} with path /etc/monit/plugins/${plugin}.sh "${argument}" "${description}" "${environment}" "${name}" every ${interval} cycles
 	if changed status then exec "/etc/monit/handlers/${notify}.sh"
 	if status != 0 then alert
