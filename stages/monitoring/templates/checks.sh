3ae5ee48f7d36ecfcc2dc8bc0dd6cd509ced9ce9
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-12 16:58:25 +0100

    Debug

--- stages/monitoring/templates/checks.sh
+++ stages/monitoring/templates/checks.sh
@@ -3,6 +3,7 @@
 for check in $(jq -cr '.checks_templates' ${1}); do
     source <(echo "${check}" | jq '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     let "interval = interval * 60"
+    echo ${name}
     cat << EOF > /etc/monit/conf.d/${name}.conf
 check program ${name} with path /etc/monit/plugins/${plugin}.sh "${argument }" "${description}" "${environment}" "${name}" every ${interval} cycles
 	if changed status then exec "/etc/monit/handlers/${notify}.sh"
