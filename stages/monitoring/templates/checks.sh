d594d6b99ec4bec04087ce72b9c0c887ad4eebba
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-12 20:04:32 +0100

    Fixed indentation

--- stages/monitoring/templates/checks.sh
+++ stages/monitoring/templates/checks.sh
@@ -7,7 +7,7 @@ for check in $(jq -cr '.checks_templates[]' ${1}); do
     let "interval = interval * 60"
     echo "Check name: ${name}"
     if [[ ${state} = 'present' ]]; then
-    cat << EOF > /etc/monit/conf.d/${name}.cfg
+        cat << EOF > /etc/monit/conf.d/${name}.cfg
 check program ${name} with path /etc/monit/plugins/${plugin}.sh "${argument}" "${description}" "${environment}" "${name}" every ${interval} cycles
 	if changed status then exec "/etc/monit/handlers/${notify}.sh"
 	if status != 0 then alert
