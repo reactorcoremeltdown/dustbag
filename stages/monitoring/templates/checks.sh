e9499bd288bbf7f723e3e66e059fc59488757cdf
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-12 17:14:35 +0100

    Cleaner debug

--- stages/monitoring/templates/checks.sh
+++ stages/monitoring/templates/checks.sh
@@ -4,11 +4,10 @@ IFS=$'\n'
 
 for check in $(jq -cr '.checks_templates[]' ${1}); do
     source <(echo "${check}" | jq '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
-    echo "${check}"
     let "interval = interval * 60"
-    echo ${name}
+    echo "Check name: ${name}"
     cat << EOF > /etc/monit/conf.d/${name}.conf
-check program ${name} with path /etc/monit/plugins/${plugin}.sh "${argument }" "${description}" "${environment}" "${name}" every ${interval} cycles
+check program ${name} with path /etc/monit/plugins/${plugin}.sh "${argument}" "${description}" "${environment}" "${name}" every ${interval} cycles
 	if changed status then exec "/etc/monit/handlers/${notify}.sh"
 	if status != 0 then alert
 EOF
