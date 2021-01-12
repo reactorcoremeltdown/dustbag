08b40c12bb587b2672157c490e69950dd4da73b7
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-12 15:18:58 +0100

    Added monitoring steps

--- stages/monitoring/monitoring.mk
+++ stages/monitoring/monitoring.mk
@@ -2,6 +2,10 @@ monitoring: monit checks
 	@echo "Not implemented yet"
 
 monit:
+	apt-get install -y monit
+	cp -pR stages/monitoring/files/plugins /etc/monit/
+	cp -pR stages/monitoring/files/handlers /etc/monit/
+	cp -pR stages/monitoring/files/scripts /etc/monit/
 	@echo "Not implemented yet"
 
 checks:
