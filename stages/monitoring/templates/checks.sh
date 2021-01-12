e1caca529f0f635fe8d30961a3a9ac12170086a1
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-12 20:02:40 +0100

    Respecting the state field

--- stages/monitoring/templates/checks.sh
+++ stages/monitoring/templates/checks.sh
@@ -6,9 +6,13 @@ for check in $(jq -cr '.checks_templates[]' ${1}); do
     source <(echo "${check}" | jq  -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     let "interval = interval * 60"
     echo "Check name: ${name}"
+    if [[ ${state} = 'present' ]]; then
     cat << EOF > /etc/monit/conf.d/${name}.cfg
 check program ${name} with path /etc/monit/plugins/${plugin}.sh "${argument}" "${description}" "${environment}" "${name}" every ${interval} cycles
 	if changed status then exec "/etc/monit/handlers/${notify}.sh"
 	if status != 0 then alert
 EOF
+    else
+        rm -frv /etc/monit/conf.d/${name}.cfg
+    fi
 done
