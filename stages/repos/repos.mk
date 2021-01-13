642589e97d7b7d7121a74cc4aa8701a09c787f81
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-14 00:58:38 +0100

    Faster gpg key import procedure

--- stages/repos/repos.mk
+++ stages/repos/repos.mk
@@ -1,10 +1,7 @@
 repos: setup_debian_repositories update_debian_repositories
 	@printf "`tput bold`Setting up Debian repositories completed`tput sgr0`\n"
 
-setup_gpg_keys:
-	jq -cr '.debian.repositories[].key' variables/main.json | xargs apt-key adv --fetch-keys --no-tty
-
-setup_debian_repositories: setup_gpg_keys
+setup_debian_repositories:
 	bash stages/repos/templates/repo.sh variables/main.json
 
 update_debian_repositories:
--- stages/repos/templates/repo.sh
+++ stages/repos/templates/repo.sh
@@ -4,11 +4,12 @@ IFS=$'\n'
 DISTRO=`lsb_release -cs`
 
 for repo in `jq -c '.debian.repositories[]' ${1}`; do
-    STATE=`echo ${repo} | jq -r '.state'`
-    REPONAME=`echo ${repo} | jq -r '.name'`
-    if [[ ${STATE} = 'present' ]]; then
-        echo "${repo}" | jq -cr '. | "deb \(.url) DISTRO \(.section)"' | sed "s|DISTRO|${DISTRO}|g" > /etc/apt/sources.list.d/${REPONAME}.list
+    source <(echo "${repo}" | jq  -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
+    KEYRING="/etc/apt/trusted.gpg.d/${name}.gpg"
+    if [[ ${state} = 'present' ]]; then
+        test -f ${KEYRING} || apt-key --keyring ${KEYRING} adv --fetch-keys --no-tty ${key}
+        echo "${repo}" | jq -cr '. | "deb \(.url) DISTRO \(.section)"' | sed "s|DISTRO|${DISTRO}|g" > /etc/apt/sources.list.d/${name}.list
     else
-        rm -f /etc/apt/sources.list.d/${REPONAME}.list || true
+        rm -fv /etc/apt/sources.list.d/${name}.list ${KEYRING} || true
     fi
 done
