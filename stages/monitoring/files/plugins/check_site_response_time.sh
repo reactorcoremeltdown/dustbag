c2a977c85d424510b8a2c046b5ef18ed7c644e3d
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-18 15:17:27 +0100

    Added result line to the output

--- stages/monitoring/files/plugins/check_site_response_time.sh
+++ stages/monitoring/files/plugins/check_site_response_time.sh
@@ -16,5 +16,5 @@ RESULT=$(echo "scale=2; ${SPENT} / 100" | bc | awk '{printf "%f", $0}')
 if [[ $(echo "${RESULT} > 2" | bc) -lt 1 ]]; then
     ok "site's response time is below 2 seconds"
 else
-    fail "site's response time went above 2 seconds!"
+    fail "site's response time went above 2 seconds: ${RESULT}"
 fi
