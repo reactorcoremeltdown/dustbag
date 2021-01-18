0eeccc0a0e683135c047898cbf2cabb5e1a7f655
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-18 15:05:23 +0100

    It should be the other way around, LOL

--- stages/monitoring/files/plugins/check_site_response_time.sh
+++ stages/monitoring/files/plugins/check_site_response_time.sh
@@ -13,7 +13,7 @@ done
 
 RESULT=$(echo "scale=2; ${SPENT} / 100" | bc | awk '{printf "%f", $0}')
 
-if [[ $(echo "${RESULT} > 2" | bc) -gt 0 ]]; then
+if [[ $(echo "${RESULT} > 2" | bc) -lt 1 ]]; then
     ok "site's response time is below 2 seconds"
 else
     fail "site's response time went above 2 seconds!"
