3dfdc5804a31efe8fd8f5e2adf97ebf43a3b7e64
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-18 14:40:53 +0100

    Added a plugin to track site's response time

---
+++ stages/monitoring/files/plugins/check_site_response_time.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+source ${PLUGINSDIR}/okfail.sh
+
+IFS=$'\n'
+SPENT="0.0"
+
+
+for entry in $(tail -n 100 ${1}); do
+    REQTIME=$(echo "${entry}" | jq -r '.fields.request_time')
+    SPENT=$(echo "${SPENT} + ${REQTIME}" | bc)
+done
+
+RESULT=$(echo "scale=2; ${SPENT} / 100" | bc | awk '{printf "%f", $0}')
+
+if [[ $(echo "${RESULT} > 2" | bc) -gt 0 ]]; then
+    ok "site's response time is below 2 seconds"
+else
+    fail "site's response time went above 2 seconds!"
+fi
