d4e9ffdc7e161a2ddffffc0c61a5c931ae94637c
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-18 19:20:24 +0100

    Double backslash maybe

--- stages/monitoring/files/handlers/gotify.sh
+++ stages/monitoring/files/handlers/gotify.sh
@@ -3,17 +3,17 @@
 bot_token=`cat /etc/datasources/gotify.json | jq -r .token`
 
 text=""
-tail="\nHostname: $HOSTNAME\nCheck name: $NAME\nDescription: $MESSAGE"
+tail="\\nHostname: $HOSTNAME\\nCheck name: $NAME\\nDescription: $MESSAGE"
 
 case $STATUS in
   "0")
-    text="✅✅✅\n$tail"
+    text="✅✅✅\\n$tail"
     ;;
   "1")
-    text="⚠️⚠️⚠️\n$tail"
+    text="⚠️⚠️⚠️\\n$tail"
     ;;
   "2")
-    text="❌❌❌\n$tail"
+    text="❌❌❌\\n$tail"
     ;;
 esac
 
