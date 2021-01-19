94b2bbab6a43513eadaea057ea6a4c55ea4caacf
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-19 22:55:37 +0100

    Reduced the number of samples

--- stages/monitoring/files/plugins/check_site_response_time.sh
+++ stages/monitoring/files/plugins/check_site_response_time.sh
@@ -6,12 +6,12 @@ IFS=$'\n'
 SPENT="0.0"
 
 
-for entry in $(tail -n 100 ${1}); do
+for entry in $(tail -n 20 ${1}); do
     REQTIME=$(echo "${entry}" | jq -r '.fields.request_time')
     SPENT=$(echo "${SPENT} + ${REQTIME}" | bc)
 done
 
-RESULT=$(echo "scale=2; ${SPENT} / 100" | bc | awk '{printf "%f", $0}')
+RESULT=$(echo "scale=2; ${SPENT} / 20" | bc | awk '{printf "%f", $0}')
 
 if [[ $(echo "${RESULT} > 2" | bc) -lt 1 ]]; then
     ok "site's response time is below 2 seconds"
