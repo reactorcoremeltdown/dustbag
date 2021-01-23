e63d89273796f92738f279cb9cf0dcc7064aaa59
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-24 00:16:03 +0100

    Useradd is under /sbin

--- stages/users/templates/users.sh
+++ stages/users/templates/users.sh
@@ -5,7 +5,7 @@ IFS=$'\n'
 for user in `jq -cr '.users[]' ${1}`; do
     source <(echo "${user}" | jq  -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     if ! groups ${name} > /dev/null; then
-        useradd -s ${shell} ${name}
+        /sbin/useradd -s ${shell} ${name}
         if [[ ${keygen} = 'true' ]]; then
             su ${name} -c 'ssh-keygen -b 2048 -t rsa q -N ""'
         fi
