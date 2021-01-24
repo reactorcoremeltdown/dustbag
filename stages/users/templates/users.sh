4ae0d8fac950358a5ddd19067db18f840e5f8622
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-24 22:42:48 +0100

    Simplified user creation

--- stages/users/templates/users.sh
+++ stages/users/templates/users.sh
@@ -4,16 +4,12 @@ IFS=$'\n'
 
 for user in `jq -cr '.users[]' ${1}`; do
     source <(echo "${user}" | jq  -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
-    NEED_USER=0
-    groups ${name} > /dev/null || NEED_USER=1
-    if ! [[ ${NEED_USER} = '1' ]]; then
-        /sbin/useradd -s ${shell} ${name}
-        if [[ ${keygen} = 'true' ]]; then
-            su ${name} -c 'ssh-keygen -b 2048 -t rsa q -N ""'
-        fi
+    HOMEDIR=$(getent passwd ${name} | cut -f 6 -d ':')
+    groups ${name} > /dev/null || /sbin/useradd -s ${shell} ${name}
+    if [[ ${keygen} = 'true' ]]; then
+        test -f ${HOMEDIR}/.ssh/id_rsa || su ${name} -c 'ssh-keygen -b 2048 -t rsa q -N ""'
     fi
     if [[ ${authorized_keys} = 'true' ]]; then
-        HOMEDIR=$(getent passwd ${name} | cut -f 6 -d ':')
         install -d -m 700 \
             -g ${name} -o ${name} \
             ${HOMEDIR}/.ssh
