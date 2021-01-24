569895ac7a44c42c071ec623a710a044df48e312
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-24 22:47:17 +0100

    Debugging user creation

--- stages/users/templates/users.sh
+++ stages/users/templates/users.sh
@@ -5,7 +5,7 @@ IFS=$'\n'
 for user in `jq -cr '.users[]' ${1}`; do
     source <(echo "${user}" | jq  -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     HOMEDIR=$(getent passwd ${name} | cut -f 6 -d ':')
-    groups ${name} > /dev/null || /sbin/useradd -s ${shell} ${name}
+    #groups ${name} > /dev/null || /sbin/useradd -s ${shell} ${name}
     if [[ ${keygen} = 'true' ]]; then
         test -f ${HOMEDIR}/.ssh/id_rsa || su ${name} -c 'ssh-keygen -b 2048 -t rsa q -N ""'
     fi
