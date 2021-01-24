178fd42fef4422f6865f61a6f2e3db9c2e2dc04c
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-24 23:01:50 +0100

    Uncommented some lines

--- stages/users/templates/users.sh
+++ stages/users/templates/users.sh
@@ -5,14 +5,14 @@ IFS=$'\n'
 for user in `jq -cr '.users[]' ${1}`; do
     source <(echo "${user}" | jq  -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     HOMEDIR=$(getent passwd ${name} | cut -f 6 -d ':')
-    #groups ${name} > /dev/null || /sbin/useradd -s ${shell} ${name}
+    groups ${name} > /dev/null || /sbin/useradd -s ${shell} ${name}
     if [[ ${keygen} = 'true' ]]; then
         test -f ${HOMEDIR}/.ssh/id_rsa || su ${name} -c 'ssh-keygen -b 2048 -t rsa q -N ""'
     fi
     if [[ ${authorized_keys} = 'true' ]]; then
-        #install -d -m 700 \
-        #    -g ${name} -o ${name} \
-        #    ${HOMEDIR}/.ssh
+        install -d -m 700 \
+            -g ${name} -o ${name} \
+            ${HOMEDIR}/.ssh
         install -D -v -m 600 \
             -g ${name} -o ${name} \
             stages/users/files/authorized_keys ${HOMEDIR}/.ssh
