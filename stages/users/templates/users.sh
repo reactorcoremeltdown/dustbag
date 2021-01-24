a549dff811a3bd9d9048a3491bbd69654435ed02
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-24 22:36:31 +0100

    Always install authorized_keys

--- stages/users/templates/users.sh
+++ stages/users/templates/users.sh
@@ -4,20 +4,21 @@ IFS=$'\n'
 
 for user in `jq -cr '.users[]' ${1}`; do
     source <(echo "${user}" | jq  -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
-    if ! groups ${name} > /dev/null; then
+    NEED_USER=0
+    groups ${name} > /dev/null || NEED_USER=1
+    if ! [[ ${NEED_USER} = '1' ]]; then
         /sbin/useradd -s ${shell} ${name}
         if [[ ${keygen} = 'true' ]]; then
             su ${name} -c 'ssh-keygen -b 2048 -t rsa q -N ""'
         fi
-        if [[ ${authorized_keys} = 'true' ]]; then
-            HOMEDIR=$(getent passwd ${name} | cut -f 6 -d ':')
-            install -d -m 700 \
-                -g ${name} -o ${name} \
-                ${HOMEDIR}/.ssh
-            install -D -v -m 600 \
-                -g ${name} -o ${name} \
-                stages/users/files/authorized_keys ${HOMEDIR}/.ssh
-        fi
     fi
-    #echo "${groups}"
+    if [[ ${authorized_keys} = 'true' ]]; then
+        HOMEDIR=$(getent passwd ${name} | cut -f 6 -d ':')
+        install -d -m 700 \
+            -g ${name} -o ${name} \
+            ${HOMEDIR}/.ssh
+        install -D -v -m 600 \
+            -g ${name} -o ${name} \
+            stages/users/files/authorized_keys ${HOMEDIR}/.ssh
+    fi
 done
