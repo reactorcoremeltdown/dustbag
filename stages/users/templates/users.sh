6f31306a5a7495b6a3c384c2edf5ed403d52cf0a
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-01-26 13:59:35 +0100

    Added chsh and usermod commands to the users template script

--- stages/users/templates/users.sh
+++ stages/users/templates/users.sh
@@ -6,6 +6,7 @@ for user in `jq -cr '.users[]' ${1}`; do
     source <(echo "${user}" | jq  -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     HOMEDIR=$(getent passwd ${name} | cut -f 6 -d ':')
     groups ${name} > /dev/null || /sbin/useradd -s ${shell} ${name}
+    chsh -s ${shell} ${name}
     if [[ ${keygen} = 'true' ]]; then
         test -f ${HOMEDIR}/.ssh/id_rsa || su ${name} -c "ssh-keygen -b 2048 -t rsa -f \"${HOMEDIR}/.ssh/id_rsa\" -q -N \"\""
     fi
@@ -17,5 +18,5 @@ for user in `jq -cr '.users[]' ${1}`; do
             -g ${name} -o ${name} \
             stages/users/files/authorized_keys ${HOMEDIR}/.ssh
     fi
-    echo ${groups}
+    usermod -G ${groups} ${name}
 done
