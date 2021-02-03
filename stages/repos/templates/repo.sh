012ad5c67d664f189c3350a855409b0fda51e677
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-02-03 15:09:07 +0100

    Made workarounds for backports

--- stages/repos/templates/repo.sh
+++ stages/repos/templates/repo.sh
@@ -7,8 +7,9 @@ for repo in `jq -c '.debian.repositories[]' ${1}`; do
     source <(echo "${repo}" | jq  -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     KEYRING="/etc/apt/trusted.gpg.d/${name}.gpg"
     if [[ ${state} = 'present' ]]; then
+        test -z ${distro} && distro=${DISTRO}
         test -f ${KEYRING} || apt-key --keyring ${KEYRING} adv --fetch-keys --no-tty ${key}
-        echo "${repo}" | jq -cr '. | "deb \(.url) DISTRO \(.section)"' | sed "s|DISTRO|${DISTRO}|g" > /etc/apt/sources.list.d/${name}.list
+        echo "${repo}" | jq -cr '. | "deb \(.url) DISTRO \(.section)"' | sed "s|DISTRO|${distro}|g" > /etc/apt/sources.list.d/${name}.list
     else
         rm -fv /etc/apt/sources.list.d/${name}.list ${KEYRING} || true
     fi
