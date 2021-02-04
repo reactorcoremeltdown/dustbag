a303fc72d781ae5dcc0a9f3a7c1cb1c3192653ae
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-02-04 18:14:42 +0100

    Maybe there's no need in suffix

--- stages/last/last.mk
+++ stages/last/last.mk
@@ -2,4 +2,4 @@ last: laminar_restart
 	@echo "Last stage completed"
 
 laminar_restart:
-	echo "systemctl restart laminar.service" | at 'now + 10 seconds'
+	echo "systemctl restart laminar.service" | at 'now + 10'
