4dfb36d9394232e0efde389cb014053f1a832c2f
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-02-04 19:14:24 +0100

    Being fancy

--- stages/last/last.mk
+++ stages/last/last.mk
@@ -1,5 +1,5 @@
 last: laminar_restart
-	@echo "$(ccgreen)Last stage completed$(ccend)"
+	@echo "$(cccyan)Last stage completed$(ccend)"
 
 laminar_restart:
 	echo "sleep 10 && systemctl restart laminar.service" | at now
