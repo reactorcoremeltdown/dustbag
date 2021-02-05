6fcb1d075fc554c917f8c61baf0d9d83ecb09fe3
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-02-05 22:30:51 +0100

    Making debug output

--- stages/services/templates/davfs2/secrets.sh
+++ stages/services/templates/davfs2/secrets.sh
@@ -5,6 +5,7 @@ IFS=$'\n'
 echo "## Davfs2 Secrets, managed by dustbag" > /etc/davfs2/secrets
 for i in $(jq -cr '.secrets.davfs2.accounts[]' /etc/secrets/secrets.json); do
     source <(echo "${i}" | jq '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
+    echo ${mountpoint}
     echo "${mountpoint} ${username} ${password}" >> /etc/davfs2/secrets
 done
 
