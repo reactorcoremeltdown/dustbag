8bd49e0f38eaffc2dcc5c2e3b1f3279408b991ad
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-02-05 22:22:54 +0100

    Wrong path to davfs2 secrets

--- stages/services/templates/davfs2/secrets.sh
+++ stages/services/templates/davfs2/secrets.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "## Davfs2 Secrets, managed by dustbag" > /etc/davfs2/secrets
-for i in $(jq -cr '.davfs2.accounts' /etc/secrets/secrets.json); do
+for i in $(jq -cr '.secrets.davfs2.accounts' /etc/secrets/secrets.json); do
     source <(echo "${i}" | jq '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     echo "${mountpoint} ${username} ${password}" >> /etc/davfs2/secrets
 done
