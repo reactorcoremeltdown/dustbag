b5681e2f9928a555ed74ad126c9e0cdc0d70fc1c
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-02-05 22:40:23 +0100

    Forgot -cr flags for JQ

--- stages/services/templates/davfs2/secrets.sh
+++ stages/services/templates/davfs2/secrets.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+
 IFS=$'\n'
 
 echo "## Davfs2 Secrets, managed by dustbag" > /etc/davfs2/secrets
 for i in $(jq -cr '.secrets.davfs2.accounts[]' /etc/secrets/secrets.json); do
-    source <(echo "${i}" | jq '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
-    echo ${mountpoint}
+    source <(echo "${i}" | jq -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     echo "${mountpoint} ${username} ${password}" >> /etc/davfs2/secrets
 done
 
