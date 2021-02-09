d852b154d6674dc23a3e6314d3fdd2abd66684ef
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-02-09 15:44:56 +0100

    Made gitconfig persistent for root user

--- stages/users/users.mk
+++ stages/users/users.mk
@@ -1,7 +1,11 @@
-users: early sudoers
+users: early sudoers configs
 	bash stages/users/templates/users.sh variables/main.json
 	@echo "$(ccgreen)Setting up users completed$(ccend)"
 
 sudoers:
 	install -D -v -m 440 \
 	stages/users/files/etc/sudoers.d/* /etc/sudoers.d/
+
+configs:
+	install -D -v -m 640 \
+	stages/users/files/root/.gitconfig /root/
