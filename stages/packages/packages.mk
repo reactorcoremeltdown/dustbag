915e0e66e1d32605cfcee4ec9ef1035498d1202b
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-02-16 17:46:25 +0100

    Splitting variables: packages get their own JSON

--- stages/packages/packages.mk
+++ stages/packages/packages.mk
@@ -1,5 +1,5 @@
-PACKAGES := $(shell jq -cr '.debian.packages[]' variables/main.json | xargs)
-PACKAGES_LITE := $(shell jq -cr '.debian.packages_lite[]' variables/main.json | xargs)
+PACKAGES := $(shell jq -cr '.debian.packages[]' stages/packages/variables/packages.json | xargs)
+PACKAGES_LITE := $(shell jq -cr '.debian.packages_lite[]' stages/packages/variables/packages.json | xargs)
 
 packages: repos
 	dpkg-query -s $(PACKAGES) > /dev/null || apt-get install -y $(PACKAGES)
