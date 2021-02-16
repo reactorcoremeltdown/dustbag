dad143a9ccffd0d0b4d1edcc4388dbaf84bf89e1
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-02-16 17:53:46 +0100

    Splitting variables: moved debian repositories to its own JSON

--- stages/repos/repos.mk
+++ stages/repos/repos.mk
@@ -2,7 +2,7 @@ repos: setup_debian_repositories update_debian_repositories
 	@echo "$(ccgreen)Setting up Debian repositories completed$(ccend)"
 
 setup_debian_repositories:
-	bash stages/repos/templates/repo.sh variables/main.json
+	bash stages/repos/templates/repo.sh stages/repos/variables/repos.json
 
 update_debian_repositories:
 	apt update
