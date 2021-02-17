226f4071cc61bcff3f21aeb1b1117d5eda22496f
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-02-17 10:20:35 +0100

    Send safe place login messages to syslog

--- stages/services/files/etc/ssh/login-notify.sh
+++ stages/services/files/etc/ssh/login-notify.sh
@@ -6,8 +6,15 @@ if [ "$PAM_TYPE" != "close_session" ]; then
     host="`hostname`"
     text="SSH Login: $PAM_USER from $PAM_RHOST on $host"
 
-    curl -s -XPOST \
-        --header "Content-Type: application/json" \
-        "https://notifications.rcmd.space/message?token=${bot_token}" \
-        --data "{ \"message\": \"${text}\", \"title\": \"SSHD\", \"priority\": 5 }"
+    case ${PAM_RHOST} in
+    "46.88.65.90" | "2a02:2770:3:0:21a:4aff:fecb:2547" )
+        echo "${text}" | logger -t sshd-login
+        ;;
+    *)
+        curl -s -XPOST \
+            --header "Content-Type: application/json" \
+            "https://notifications.rcmd.space/message?token=${bot_token}" \
+            --data "{ \"message\": \"${text}\", \"title\": \"SSHD\", \"priority\": 5 }"
+        ;;
+    esac
 fi
