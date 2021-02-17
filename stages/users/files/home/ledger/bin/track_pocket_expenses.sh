802d3c6bcead789ac46d598c79b19e0b61208caf
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-02-17 15:04:11 +0100

    Updated a script for tracking pocket expenses

---
+++ stages/users/files/home/ledger/bin/track_pocket_expenses.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+# Getting a job token
+USER_TOKEN=$(cat /home/ledger/.token)
+QUEUE="ledger"
+GET_JOB_TOKEN=$(curl -s -XPOST --data-urlencode "token=${USER_TOKEN}" "https://api.rcmd.space/v5/token/get")
+JOB_ID=$(curl -s -XPOST \
+    --data-urlencode "token=${GET_JOB_TOKEN}" \
+    --data-urlencode "queue=${QUEUE}" \
+    "https://api.rcmd.space/v5/queue/get-job")
+
+if [[ ${JOB_ID} != 'EOQ' ]]; then
+    sleep 1
+    echo "Getting job Payload"
+    GET_JOB_PAYLOAD_TOKEN=$(curl -s -XPOST --data-urlencode "token=${USER_TOKEN}" https://api.rcmd.space/v5/token/get)
+    JOB_PAYLOAD=$(curl -s -XPOST \
+                --data-urlencode "token=${GET_JOB_PAYLOAD_TOKEN}" \
+                --data-urlencode "queue=${QUEUE}" \
+                --data-urlencode "job=${JOB_ID}" \
+                "https://api.rcmd.space/v5/queue/get-job-payload")
+    sleep 1
+
+    echo "Locking job"
+    LOCK_JOB_TOKEN=$(curl -s -XPOST --data-urlencode "token=${USER_TOKEN}" https://api.rcmd.space/v5/token/get)
+    curl -s -XPOST \
+        --data-urlencode "token=${LOCK_JOB_TOKEN}" \
+        --data-urlencode "queue=${QUEUE}" \
+        --data-urlencode "job=${JOB_ID}" \
+        "https://api.rcmd.space/v5/queue/lock-job"
+
+    sleep 1
+
+    echo "Payload: ${JOB_PAYLOAD}"
+
+    SOURCE_TOPIC=$(echo "${JOB_PAYLOAD}" | jq -r '.topic.source')
+    DESTINATION_TOPIC=$(echo "${JOB_PAYLOAD}" | jq -r '.topic.destination')
+    DESCRIPTION=$(echo "${JOB_PAYLOAD}" | jq -r '.description')
+    AMOUNT=$(echo "${JOB_PAYLOAD}" | jq -r '.amount' | sed 's|\.|,|')
+    if ! test -z ${AMOUNT}; then
+            echo -e "\n$(date '+%Y/%m/%d') ${DESCRIPTION}\n    ${DESTINATION_TOPIC}  ${AMOUNT}\n    ${SOURCE_TOPIC}  -${AMOUNT}\n" >> /home/ledger/ledger.book
+
+            echo "Unlocking job"
+            UNLOCK_JOB_TOKEN=$(curl -s -XPOST --data-urlencode "token=${USER_TOKEN}" https://api.rcmd.space/v5/token/get)
+            curl -s -XPOST \
+                --data-urlencode "token=${UNLOCK_JOB_TOKEN}" \
+                --data-urlencode "queue=${QUEUE}" \
+                --data-urlencode "job=${JOB_ID}" \
+                "https://api.rcmd.space/v5/queue/unlock-job"
+
+            sleep 1
+
+            echo "Acknowledging job"
+            ACK_JOB_TOKEN=$(curl -s -XPOST --data-urlencode "token=${USER_TOKEN}" https://api.rcmd.space/v5/token/get)
+            curl -s -XPOST \
+                --data-urlencode "token=${ACK_JOB_TOKEN}" \
+                --data-urlencode "queue=${QUEUE}" \
+                --data-urlencode "job=${JOB_ID}" \
+                "https://api.rcmd.space/v5/queue/ack-job"
+
+    else
+            echo "Failed to parse transaction amount, unlocking job"
+            UNLOCK_JOB_TOKEN=$(curl -s -XPOST --data-urlencode "token=${USER_TOKEN}" https://api.rcmd.space/v5/token/get)
+            curl -s -XPOST \
+                --data-urlencode "token=${UNLOCK_JOB_TOKEN}" \
+                --data-urlencode "queue=${QUEUE}" \
+                --data-urlencode "job=${JOB_ID}" \
+                "https://api.rcmd.space/v5/queue/unlock-job"
+    fi
+fi
--- stages/users/users.mk
+++ stages/users/users.mk
@@ -1,4 +1,4 @@
-users: early sudoers configs
+users: early sudoers configs ledger_scripts
 	bash stages/users/templates/users.sh stages/users/variables/users.json
 	@echo "$(ccgreen)Setting up users completed$(ccend)"
 
@@ -9,3 +9,9 @@ sudoers:
 configs:
 	install -D -v -m 640 \
 	stages/users/files/root/.gitconfig /root/
+
+ledger_scripts:
+	jq -cr '.secrets.ledger.token' > /home/ledger/.token
+	chmod 600 /home/ledger/.token && chown ledger:ledger /home/ledger/.token
+	install -d /home/ledger/bin
+	install -D -v -m 755 stages/users/files/home/ledger/bin/track_pocket_expenses.sh /home/ledger/bin
