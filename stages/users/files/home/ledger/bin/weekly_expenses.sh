36324c77d2cb1edacf175d281550e8fb634d2b5e
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-03-10 23:30:48 +0100

    fixed curl call

--- stages/users/files/home/ledger/bin/weekly_expenses.sh
+++ stages/users/files/home/ledger/bin/weekly_expenses.sh
@@ -4,4 +4,4 @@ BOT_TOKEN=`cat /home/ledger/.config/telegram_token`
 CHAT_ID=`cat /home/ledger/.config/telegram_chat_id`
 
 hledger balance --depth 1 --weekly --begin=$(date --date="today - 3 weeks" "+%Y/%m/%d") | convert label:@- /home/ledger/expenses.png
-curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto" -F chat_id="${CHAT_ID}" -F "@/home/ledger/expenses.png"
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto" -F chat_id="${CHAT_ID}" -F "photo=@/home/ledger/expenses.png"
