6f1dd65466b21bd175b678fda2775f4fd10ed222
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-03-29 21:02:43 +0200

    Fixed bad copy-pasta

--- stages/users/templates/cloudflare.sh
+++ stages/users/templates/cloudflare.sh
@@ -3,7 +3,7 @@
 IFS=$'\n'
 
 dns_cloudflare_email=$(jq -cr '.secrets.cloudflare.dns_cloudflare_email' /etc/secrets/secrets.json)
-dns_cloudflare_email=$(jq -cr '.secrets.cloudflare.dns_cloudflare_api_key' /etc/secrets/secrets.json)
+dns_cloudflare_api_key=$(jq -cr '.secrets.cloudflare.dns_cloudflare_api_key' /etc/secrets/secrets.json)
 
 cat << EOF > /root/.cloudflare.ini
 dns_cloudflare_email = "${dns_cloudflare_email}"
