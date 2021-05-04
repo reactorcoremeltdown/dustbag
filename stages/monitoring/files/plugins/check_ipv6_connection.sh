80756002f327eb818fff06f81e5a9eab979a1f00
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-05-04 18:42:59 +0200

    Added a check for monitoring IPv6 reachability

---
+++ stages/monitoring/files/plugins/check_ipv6_connection.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source /etc/monitoring/plugins/okfail.sh
+
+status=`curl --head -A "monit-ping-check" -6 -s -o /dev/null -w "%{http_code}" --connect-timeout 10 --max-time 10 $1`
+
+if [[ $status == "200" ]]; then
+    ok "IPv6 is reachable"
+else
+    fail "IPv6 link is DOWN"
+fi
