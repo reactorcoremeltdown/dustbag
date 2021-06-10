eb4fafd3f4a1cf7be8eb1f3ea6ad388e500746b9
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-06-10 16:15:35 +0200

    This line also needs to be commented out

--- stages/users/users.mk
+++ stages/users/users.mk
@@ -17,7 +17,7 @@ ledger_scripts:
 #	jq -cr '.secrets.ledger.token' /etc/secrets/secrets.json > /home/ledger/.token
 #	jq -cr '.secrets.telegram.bot_token' /etc/secrets/secrets.json > /home/ledger/.config/telegram_token
 #	jq -cr '.secrets.telegram.chat_id' /etc/secrets/secrets.json > /home/ledger/.config/telegram_chat_id
-	chmod 600 /home/ledger/.token && chown ledger:ledger /home/ledger/.token /home/ledger/.config/telegram_token /home/ledger/.config/telegram_chat_id
+#	chmod 600 /home/ledger/.token && chown ledger:ledger /home/ledger/.token /home/ledger/.config/telegram_token /home/ledger/.config/telegram_chat_id
 	install -d -m 775 --owner root --group apps /var/spool/api/ledger
 	install -d /home/ledger/bin
 	install -D -v -m 755 stages/users/files/home/ledger/bin/track_pocket_expenses.sh /home/ledger/bin
