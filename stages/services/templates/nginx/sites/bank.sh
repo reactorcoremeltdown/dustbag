3195d744aa79189fd0b7da2c0e4972bcdc01c94b
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-11-06 03:06:29 +0100

    Replaced basic auth with client certificate auth

--- stages/services/templates/nginx/sites/bank.sh
+++ stages/services/templates/nginx/sites/bank.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+SITE='bank'
+
 source <(jq -r '.nginx.variables | to_entries[] | [.key,(.value|@sh)] | join("=")' variables/main.json)
 
 cat <<EOF > /etc/nginx/sites-available/bank.conf
 server {
   listen 80;
   listen [::]:80;
-  server_name bank.rcmd.space;
+  server_name ${site}.rcmd.space;
 
   return 301 https://\$server_name\$request_uri;
 }
@@ -15,8 +17,8 @@ server {
     listen 443 ssl http2;
     listen [::]:443 ssl http2;
 
-    access_log /var/log/nginx/bank.rcmd.space_access.log json;
-    error_log /var/log/nginx/bank.rcmd.space_error.log;
+    access_log /var/log/nginx/${SITE}.rcmd.space_access.log json;
+    error_log /var/log/nginx/${SITE}.rcmd.space_error.log;
 
     ### SSL cert files ###
     ssl_certificate ${new_ssl_certificate};
@@ -29,6 +31,10 @@ server {
     ssl_ciphers '${ssl_ciphers}';
     ssl_prefer_server_ciphers on;
 
+    ssl_client_certificate /etc/nginx/ssl/ca.crt;
+    ssl_verify_client optional;
+    ssl_verify_depth 2;
+
     ### Compression
     gzip on;
     gzip_comp_level    5;
@@ -39,16 +45,19 @@ server {
     application/rss+xml
     text/css;
 
-    server_name bank.rcmd.space;
+    server_name ${site}.rcmd.space;
 
-    auth_basic "Protected area";
-    auth_basic_user_file /etc/nginx/htpasswd;
+    # auth_basic "Protected area";
+    # auth_basic_user_file /etc/nginx/htpasswd;
 
     location / {
+        if (\$ssl_client_verify != SUCCESS) {
+            return 403;
+        }
         proxy_pass http://127.0.0.1:55000;
     }
 }
 
 EOF
 
-ln -sf /etc/nginx/sites-available/bank.conf /etc/nginx/sites-enabled/bank.conf
+ln -sf /etc/nginx/sites-available/${SITE}.conf /etc/nginx/sites-enabled/${SITE}.conf
