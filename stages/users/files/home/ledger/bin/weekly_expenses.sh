09e4b4eeae28c8bc143ad752eb9a8331daff77c6
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-11-13 08:08:13 +0100

    Updated expenses script

--- stages/users/files/home/ledger/bin/weekly_expenses.sh
+++ stages/users/files/home/ledger/bin/weekly_expenses.sh
@@ -3,5 +3,9 @@
 BOT_TOKEN=`cat /home/ledger/.config/telegram_token`
 CHAT_ID=`cat /home/ledger/.config/telegram_chat_id`
 
-hledger -f /home/ledger/ledger.book balance --depth 2 --weekly --begin=$(date --date="today - 3 weeks" "+%Y/%m/%d") | convert -font /usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf -pointsize 24 -resize 1024x768 label:@- /home/ledger/expenses.png
-curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto" -F chat_id="${CHAT_ID}" -F "photo=@/home/ledger/expenses.png"
+hledger -f /home/ledger/ledger.book balance --depth 2 --weekly --begin=$(date --date="today - 3 weeks" "+%Y/%m/%d") > /home/ledger/report.txt
+
+convert -font /usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf -pointsize 24 -resize 1024x768 $(cat /home/ledger/report.txt) /home/ledger/report.png
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto" -F chat_id="${CHAT_ID}" -F "photo=@/home/ledger/report.png"
+
+rm -f /home/ledger/report.txt /home/ledger/report.png
