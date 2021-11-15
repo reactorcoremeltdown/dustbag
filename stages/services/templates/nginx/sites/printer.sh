b070ec246af47762aaaab932806556832692e974
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-11-15 16:44:08 +0100

    Copy-pasted nginx config from https://anthe.studio/blog/en/cups-nginx-reverse-proxy

--- stages/services/templates/nginx/sites/printer.sh
+++ stages/services/templates/nginx/sites/printer.sh
@@ -46,15 +46,13 @@ server {
     location / {
         proxy_pass http://127.0.0.1:631;
         proxy_http_version 1.1;
+        proxy_set_header Accept-Encoding "";
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host '127.0.0.1';
+        proxy_cache_bypass \$http_upgrade;
 
-        # Ensuring it can use websockets
-        proxy_set_header   Upgrade \$http_upgrade;
-        proxy_set_header   Connection "upgrade";
-        proxy_set_header   X-Real-IP \$remote_addr;
-        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header   X-Forwarded-Proto http;
-        proxy_redirect     http:// \$scheme://;
-        proxy_set_header   Host localhost;
+        proxy_set_header X-Real-IP \$remote_addr;
 
         # These sets the timeout so that the websocket can stay alive
         proxy_connect_timeout   7m;
