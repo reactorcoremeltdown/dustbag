3edffdd1a247aa066c2f4e77bda2204b0a0ef834
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-11-17 17:12:28 +0100

    Revamped expenses monitoring

--- stages/users/files/home/ledger/bin/weekly_expenses.sh
+++ stages/users/files/home/ledger/bin/weekly_expenses.sh
@@ -1,10 +1,48 @@
 #!/usr/bin/env bash
 
+cat <<EOF > /home/ledger/plot.gnu
+set terminal pngcairo enhanced font "arial,10" fontscale 1.0 size 600, 400
+set output '/home/ledger/out.png'
+set style data histogram
+set xtics nomirror rotate by -45
+set style fill solid border -1
+set arrow from graph 0,first 25 to graph 1,first 25 nohead lc rgb "#FF0000" front
+plot for [i=2:5] '/dev/stdin' using i:xtic(1) title col
+EOF
+
+#!/usr/bin/env bash
+
 BOT_TOKEN=`cat /home/ledger/.config/telegram_token`
 CHAT_ID=`cat /home/ledger/.config/telegram_chat_id`
 
-hledger -f /home/ledger/ledger.book balance --depth 2 --weekly --begin=$(date --date="today - 3 weeks" "+%Y/%m/%d") > /home/ledger/report.txt
+IFS=$'\n'
 
-curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?parse_mode=html&chat_id=${CHAT_ID}" -H "Content-Type: application/json" -d "{ \"text\": \"<code>$(cat /home/ledger/report.txt)</code>\" }"
+textdata_food=$(mktemp)
 
-rm -f /home/ledger/report.txt
+echo -e "category\tweek_1\tweek_2\tweek_3\tweek_4" >> ${textdata_food}
+
+for i in $(hledger -f /home/ledger/ledger.book balance -O json --depth 2 --weekly --begin=$(date --date="today - 3 weeks" "+%Y/%m/%d") food | jq -cr '.prRows[]'); do
+        NAME=$(echo "${i}" | jq -cr '.prrName')
+        #echo ${i}
+        NUMBERS=$(echo "${i}" | jq -cr '.prrAmounts[][0].aquantity.floatingPoint' | sed "s|null|0|g" | tr "\n" "\t")
+        echo -e "${NAME}\t${NUMBERS}" >> ${textdata_food}
+done
+
+gnuplot -p /home/ledger/plot.gnu < ${textdata_food}
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto" -F chat_id="${CHAT_ID}" -F "photo=@/home/ledger/out.png"
+rm ${textdata_food}
+
+textdata_leisure=$(mktemp)
+
+echo -e "category\tweek_1\tweek_2\tweek_3\tweek_4" >> ${textdata_leisure}
+
+for i in $(hledger -f /home/ledger/ledger.book balance -O json --depth 2 --weekly --begin=$(date --date="today - 3 weeks" "+%Y/%m/%d") leisure | jq -cr '.prRows[]'); do
+        NAME=$(echo "${i}" | jq -cr '.prrName')
+        #echo ${i}
+        NUMBERS=$(echo "${i}" | jq -cr '.prrAmounts[][0].aquantity.floatingPoint' | sed "s|null|0|g" | tr "\n" "\t")
+        echo -e "${NAME}\t${NUMBERS}" >> ${textdata_leisure}
+done
+
+gnuplot -p /home/ledger/plot.gnu < ${textdata_leisure}
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto" -F chat_id="${CHAT_ID}" -F "photo=@/home/ledger/out.png"
+rm ${textdata_leisure}
