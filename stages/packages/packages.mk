ff4ad08cc16367454d15735aa509a8c3ec5c93d3
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-12-11 23:19:39 +0100

    Save time when installing pip packages

--- stages/packages/packages.mk
+++ stages/packages/packages.mk
@@ -6,5 +6,5 @@ endif
 
 packages: repos
 	dpkg-query -s $(PACKAGES) > /dev/null || DEBIAN_FRONTEND=noninteractive apt-get -o Acquire::ForceIPv4=true install -y $(PACKAGES)
-	pip3 install --upgrade youtube-dl tqdm==4.62.2
+	pip3 install youtube-dl tqdm==4.62.2
 	@echo "$(ccgreen)Installing Debian packages completed$(ccend)"
