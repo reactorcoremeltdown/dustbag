b70daef16cac64f9e20f966e8d6c1a61afc3be5a
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-12-11 21:44:46 +0100

    Changed quotation

--- stages/packages/packages.mk
+++ stages/packages/packages.mk
@@ -1,9 +1,10 @@
-packages: repos
-ifeq ($(MAKECMDGOALS),"")
-PACKAGES = $(shell jq -cr '.debian.packages[]' stages/packages/variables/packages.json | xargs)
+ifeq ($(MAKECMDGOALS),'')
+	PACKAGES := $(shell jq -cr '.debian.packages[]' stages/packages/variables/packages.json | xargs)
 else
-PACKAGES = $(shell jq -cr '.debian.packages_lite[]' stages/packages/variables/packages.json | xargs)
+	PACKAGES := $(shell jq -cr '.debian.packages_lite[]' stages/packages/variables/packages.json | xargs)
 endif
+
+packages: repos
 	dpkg-query -s $(PACKAGES) > /dev/null || DEBIAN_FRONTEND=noninteractive apt-get -o Acquire::ForceIPv4=true install -y $(PACKAGES)
 	pip3 install --upgrade youtube-dl tqdm==4.62.2
 	@echo "$(ccgreen)Installing Debian packages completed$(ccend)"
