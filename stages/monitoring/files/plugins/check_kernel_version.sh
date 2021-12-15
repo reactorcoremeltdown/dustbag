9ea94ad34e75bc04fea736efa712bf39185eccb8
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2021-12-15 21:55:52 +0100

    No need to edit crons

--- stages/monitoring/files/plugins/check_kernel_version.sh
+++ stages/monitoring/files/plugins/check_kernel_version.sh
@@ -7,7 +7,5 @@ current_running=`uname -v | awk '{print $4}'`
 if dpkg -l | grep linux-image | awk '{print $3}' | grep -q $current_running; then
     ok "Linux kernel is up to date." "$DESCRIPTION" "$ENVIRONMENT"
 else
-    warning "Linux kernel $current_running is OUTDATED, server restart is scheduled" "$DESCRIPTION" "$ENVIRONMENT"
-    echo "0 21 * * 6 /sbin/shutdown -r now" > /var/spool/cron/crontabs/root
-    echo "0 22 * * 6 /usr/bin/crontab -r" >> /var/spool/cron/crontabs/root 
+    warning "Linux kernel $current_running is OUTDATED, server restart needed" "$DESCRIPTION" "$ENVIRONMENT"
 fi
