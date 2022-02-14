24c5d1b3805e9a9e1bf49805aa1d5f1b231a9244
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2022-02-14 10:45:31 +0100

    Enabled deviceping check for fermium

---
+++ stages/monitoring/files/plugins/check_deviceping.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source ${PLUGINSDIR}/okfail.sh
+
+IFS=$'\n'
+
+TIMESTAMP=`cat /var/spool/api/deviceping/${1}`
+CURRENT=`date '+%s'`
+DELTA=`echo "${CURRENT} - ${TIMESTAMP}" | bc`
+
+if [[ ${DELTA} -lt 1200 ]]; then
+    ok "Device ${1} online"
+else
+    fail "Device ${1} offline"
+fi
