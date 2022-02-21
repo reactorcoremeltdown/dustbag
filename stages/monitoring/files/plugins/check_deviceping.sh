72535bf425fdd14cd592e71f3a68b1b472ccf1a6
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2022-02-21 15:22:10 +0100

    This check is giving lots of false positives, need to increase an interval & display delta

--- stages/monitoring/files/plugins/check_deviceping.sh
+++ stages/monitoring/files/plugins/check_deviceping.sh
@@ -8,8 +8,8 @@ TIMESTAMP=`cat /var/spool/api/deviceping/${1}`
 CURRENT=`date '+%s'`
 DELTA=`echo "${CURRENT} - ${TIMESTAMP}" | bc`
 
-if [[ ${DELTA} -lt 1200 ]]; then
+if [[ ${DELTA} -lt 1800 ]]; then
     ok "Device ${1} online"
 else
-    fail "Device ${1} offline"
+    fail "Device ${1} offline, delta is ${DELTA}"
 fi
