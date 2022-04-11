4659b0f6ac46562c34be4714d8e29e0bc372387c
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2022-04-11 13:18:29 +0200

    Updated disk space check configs

--- stages/monitoring/files/plugins/check_disk_space.sh
+++ stages/monitoring/files/plugins/check_disk_space.sh
@@ -7,7 +7,7 @@ usage=`df $1 | tail -n 1 | awk '{print $5}' | sed 's|%||'`
 rrdtool update /var/storage/wastebox/operations/disk_space.rrd N:${usage}
 
 if [[ $usage -lt 95 ]]; then
-    ok "enough free space on disk"
+    ok "${usage}% of free disk space"
 else
     fail "The amount of used disk space on $1 is $usage%"
 fi
