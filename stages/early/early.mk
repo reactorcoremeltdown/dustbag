5d903f74a2fd160ecc9ae30ebbc84dbe11278c01
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2022-05-03 19:07:16 +0200

    Added debian keyrings

--- stages/early/early.mk
+++ stages/early/early.mk
@@ -42,6 +42,7 @@ ifneq ($(RETRY), true)
 	apt update && DEBIAN_FRONTEND=noninteractive apt -o Acquire::ForceIPv4=true install -y dirmngr \
 		apt-transport-https \
 		certbot \
+		debian-keyring \
 		python3-certbot-dns-cloudflare \
 		locales
 endif
