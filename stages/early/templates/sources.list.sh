da4b3ac93decf78bb9d1324b603a5f34c5657021
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2022-05-16 18:46:36 +0200

    Security entries only for current releases

--- stages/early/templates/sources.list.sh
+++ stages/early/templates/sources.list.sh
@@ -7,10 +7,15 @@ DEBIAN_VERSION=`lsb_release -sr`
 cat <<EOF > /etc/apt/sources.list
 deb http://debian.mirror.iphh.net/debian ${DISTRO} main
 deb-src http://debian.mirror.iphh.net/debian ${DISTRO} main
-deb http://security.debian.org/debian-security ${DISTRO}-security main
-deb-src http://security.debian.org/debian-security ${DISTRO}-security main
 deb http://debian.mirror.iphh.net/debian ${DISTRO}-updates main
 deb-src http://debian.mirror.iphh.net/debian ${DISTRO}-updates main
 deb http://debian.mirror.iphh.net/debian ${DISTRO}-backports main
 deb-src http://debian.mirror.iphh.net/debian ${DISTRO}-backports main
 EOF
+
+if [[ ${DEBIAN_VERSION} = '11' ]]; then
+    cat <<EOF >> /etc/apt/sources.list
+deb http://security.debian.org/debian-security ${DISTRO}-security main
+deb-src http://security.debian.org/debian-security ${DISTRO}-security main
+EOF
+fi
