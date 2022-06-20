60222f5da9904c241f6f5bf28c5c14f41197c326
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2022-06-20 13:28:33 +0200

    Checking how additional env variables work

--- stages/monitoring/files/plugins/check_file.sh
+++ stages/monitoring/files/plugins/check_file.sh
@@ -3,7 +3,7 @@
 source /etc/monitoring/plugins/okfail.sh
 
 if [[ -f $1 ]]; then
-	ok "File $1 is in place." "$DESCRIPTION" "$ENVIRONMENT"
+	ok "File $1 is in place. Status: ${WARNING_THRESHOLD}, ${CRITICAL_THRESHOLD}, ${FLOW_OPERATOR}" "$DESCRIPTION" "$ENVIRONMENT"
 else
 	fail "File $1 is not in place!" "$DESCRIPTION" "$ENVIRONMENT"
 fi
