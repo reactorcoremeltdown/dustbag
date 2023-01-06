39000b3da2843c704e4807acf52afab30927fa49
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-01-07 00:48:42 +0100

    Fixed string comparison operator

--- stages/last/files/bin/prepare_drone_restart.sh
+++ stages/last/files/bin/prepare_drone_restart.sh
@@ -3,7 +3,7 @@
 CURRENT_CHECKSUM=$(md5sum /etc/drone/server.cfg)
 OLD_CHECKSUM=$(cat /etc/drone/checksum.txt)
 
-if [[ ${CURRENT_CHECKSUM} != ${OLD_CHECKSUM}]]; then
+if [ "${CURRENT_CHECKSUM}" != "${OLD_CHECKSUM}" ]; then
     echo "sleep 10 && systemctl restart drone-server.service" | at now
 else
     echo "CI server restart is not scheduled"
