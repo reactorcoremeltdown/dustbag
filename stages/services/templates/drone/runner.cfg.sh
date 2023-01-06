fbdcce425ffea37e41368ab185fa44ad856704ad
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-01-06 22:35:17 +0100

    Added labels to exec runner

--- stages/services/templates/drone/runner.cfg.sh
+++ stages/services/templates/drone/runner.cfg.sh
@@ -7,6 +7,7 @@ RPC_SECRET=$(jq -r '.secrets.drone."rpc-secret"' /etc/secrets/secrets.json)
 
 cat <<EOF > /home/git/.drone-runner-exec/config
 DRONE_RUNNER_NAME=${HOSTNAME}
+DRONE_RUNNER_LABELS=machine:${HOSTNAME}
 DRONE_RPC_PROTO=https
 DRONE_RPC_HOST=ci-beta.rcmd.space
 DRONE_RPC_SECRET=${RPC_SECRET}
