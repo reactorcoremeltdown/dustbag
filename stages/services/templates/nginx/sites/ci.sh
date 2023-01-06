a95a8f4a4bb70f8383b14dd43e515c94cb4aec76
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-01-06 19:25:38 +0100

    Updated CI vhost

--- stages/services/templates/nginx/sites/ci.sh
+++ stages/services/templates/nginx/sites/ci.sh
@@ -83,7 +83,67 @@ server {
     }
 }
 
+server {
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
+
+    access_log /var/log/nginx/${SITE}-beta.rcmd.space_access.log json;
+    error_log /var/log/nginx/${SITE}-beta.rcmd.space_error.log;
+
+    ### SSL cert files ###
+    ssl_certificate ${new_ssl_certificate};
+    ssl_certificate_key ${new_ssl_certificate_key};
+
+    ssl_client_certificate /etc/nginx/pki/pki/ca.crt;
+    ssl_crl /etc/nginx/pki/pki/crl.pem;
+    ssl_verify_client optional;
+    ssl_verify_depth 2;
+
+    ### Add SSL specific settings here ###
+    ssl_session_timeout 10m;
+
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers '${ssl_ciphers}';
+    ssl_prefer_server_ciphers on;
+
+    ### Compression
+    gzip on;
+    gzip_comp_level    5;
+    gzip_min_length    256;
+    gzip_proxied       any;
+    gzip_vary          on;
+    gzip_types
+    application/rss+xml
+    text/css;
+
+    server_name ${SITE}-beta.rcmd.space;
+
+    location / {
+
+        # auth_basic "Protected area";
+        # auth_basic_user_file /etc/nginx/htpasswd;
+        proxy_pass http://127.0.0.1:28002;
+        proxy_http_version 1.1;
+
+        # Ensuring it can use websockets
+        proxy_set_header   Upgrade \$http_upgrade;
+        proxy_set_header   Connection "upgrade";
+        proxy_set_header   X-Real-IP \$remote_addr;
+        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto http;
+        proxy_redirect     http:// \$scheme://;
+
+        # The proxy must preserve the host because gotify verifies the host with the origin
+        # for WebSocket connections
+        proxy_set_header   Host \$http_host;
+
+        # These sets the timeout so that the websocket can stay alive
+        proxy_connect_timeout   7m;
+        proxy_send_timeout      7m;
+        proxy_read_timeout      7m;
+    }
+}
+
 EOF
 
-rm -f /etc/nginx/sites-available/drone.conf || true
 ln -sf /etc/nginx/sites-available/${SITE}.conf /etc/nginx/sites-enabled/${SITE}.conf
