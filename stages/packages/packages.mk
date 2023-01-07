5c1470cca03025c98c201726e16f53d3de0be11a
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-01-07 02:16:21 +0100

    increased build speed

--- stages/packages/packages.mk
+++ stages/packages/packages.mk
@@ -1,10 +1,12 @@
 ifeq ($(MAKECMDGOALS),)
 	PACKAGES := $(shell jq -cr '.debian.packages[]' stages/packages/variables/packages.json | xargs)
+PYTHON_PACKAGES := pip3 install youtube-dl tqdm==4.62.2
 else
 	PACKAGES := $(shell jq -cr '.debian.packages_lite[]' stages/packages/variables/packages.json | xargs)
+PYTHON_PACKAGES := /bin/true
 endif
 
 packages: repos
 	dpkg-query -s $(PACKAGES) > /dev/null || DEBIAN_FRONTEND=noninteractive apt-get -o Acquire::ForceIPv4=true install -y $(PACKAGES)
-	pip3 install youtube-dl tqdm==4.62.2
+	$(PYTHON_PACKAGES)
 	@echo "$(ccgreen)Installing Debian packages completed$(ccend)"
