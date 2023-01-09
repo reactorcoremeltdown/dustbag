bbcef504179cdfc17346be2b930f53a190a89130
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-01-09 17:26:58 +0100

    Reworked packages

--- stages/packages/packages.mk
+++ stages/packages/packages.mk
@@ -1,12 +1,16 @@
 ifeq ($(MAKECMDGOALS),)
-	PACKAGES := $(shell jq -cr '.debian.packages[]' stages/packages/variables/packages.json | xargs)
-PYTHON_PACKAGES := pip3 install youtube-dl tqdm==4.62.2
+	PACKAGES := $(shell jq -cr '.debian.essentials[], .debian.server[]' stages/packages/variables/packages.json | xargs)
+	PYTHON_PACKAGES := $(shell jq -cr '.python.server[]' stages/packages/variables/packages.json | xargs)
+else ifeq($(MAKECMDGOALS), seedbox)
+	PACKAGES := $(shell jq -cr '.debian.essentials[]' stages/packages/variables/packages.json | xargs)
+	PYTHON_PACKAGES := $(shell jq -cr '.python.noop[]' stages/packages/variables/packages.json | xargs)
 else
-	PACKAGES := $(shell jq -cr '.debian.packages_lite[]' stages/packages/variables/packages.json | xargs)
+	PACKAGES := $(shell jq -cr '.debian.essentials[]' stages/packages/variables/packages.json | xargs)
+	PYTHON_PACKAGES := $(shell jq -cr '.python.noop[]' stages/packages/variables/packages.json | xargs)
 PYTHON_PACKAGES := /bin/true
 endif
 
 packages: repos
 	dpkg-query -s $(PACKAGES) > /dev/null || DEBIAN_FRONTEND=noninteractive apt-get -o Acquire::ForceIPv4=true install -y $(PACKAGES)
-	$(PYTHON_PACKAGES)
-	@echo "$(ccgreen)Installing Debian packages completed$(ccend)"
+	pip3 install $(PYTHON_PACKAGES)
+	@echo "$(ccgreen)Installing Packages completed$(ccend)"
