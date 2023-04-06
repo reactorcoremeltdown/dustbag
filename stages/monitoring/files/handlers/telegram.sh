7c6aaaca31f3594796a2f8f4bfa77cac35cf9d41
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-04-06 15:04:42 +0200

    Updated telegram messages

--- stages/monitoring/files/handlers/telegram.sh
+++ stages/monitoring/files/handlers/telegram.sh
@@ -5,16 +5,17 @@ chat_id=`cat /etc/secrets/secrets.json | jq -r .secrets.telegram.chat_id`
 
 text=""
 tail="\\nHostname: $HOSTNAME\\nCheck name: $NAME\\nDescription: $MESSAGE"
+tail_extended="\\nHostname: $HOSTNAME\\nCheck name: $NAME\\nDescription: $MESSAGE\\n\\nLogin: https://http2ssh.tiredsysadmin.cc/go.html?ssh=ssh://rcmd@${HOSTNAME}"
 
 case $STATUS in
   "0")
     text="✅✅✅\\n$tail"
     ;;
   "1")
-    text="⚠️⚠️⚠️\\n$tail"
+    text="⚠️⚠️⚠️\\n$tail_extended"
     ;;
   "2")
-    text="❌❌❌\\n$tail"
+    text="❌❌❌\\n$tail_extended"
     ;;
 esac
 
