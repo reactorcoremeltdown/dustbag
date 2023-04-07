0e9b8c61a3e013e9d6f8ba665b2af13aa74f2e86
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-04-08 00:32:55 +0200

    Added a check for Hetzner StorageBOX

---
+++ stages/monitoring/files/plugins/check_storagebox_space.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+source /etc/monitoring/plugins/okfail.sh
+
+usage=`ssh -p 23 ${1}@${1}.your-storagebox.de df -h | tail -n 1 | awk '{print $5}' | sed 's|%||'`
+
+if [[ $usage -gt ${CRITICAL_THRESHOLD} ]]; then
+    fail "The amount of used disk space on $1 is $usage%"
+elif [[ $usage -gt ${WARNING_THRESHOLD} ]]; then
+    warning "The amount of used disk space on $1 is $usage%"
+else
+    ok "${usage}% of ${1} is currently used"
+fi
