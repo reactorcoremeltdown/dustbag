37f76518a93976aab234225bf807b550e3dc57bd
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-04-16 16:13:39 +0200

    Unset filters

--- stages/services/templates/podsync/podsync.toml.sh
+++ stages/services/templates/podsync/podsync.toml.sh
@@ -29,6 +29,7 @@ for feed in $(jq -cr '.services.podsync.feeds[]' ${1}); do
     fi
     if [[ ${filters} != "" ]]; then
         echo "  filters = ${filters}" >> /etc/podsync/podsync.toml
+        unset ${filters}
     fi
 done
 
