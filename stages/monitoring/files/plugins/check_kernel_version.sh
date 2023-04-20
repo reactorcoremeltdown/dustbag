3c153579283db1c113188b5a0cbea9b54a4c8476
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-04-20 17:45:01 +0200

    Updated kernel version plugin

--- stages/monitoring/files/plugins/check_kernel_version.sh
+++ stages/monitoring/files/plugins/check_kernel_version.sh
@@ -4,7 +4,7 @@ source /etc/monitoring/plugins/okfail.sh
 
 current_running=`uname -v | awk '{print $4}'`
 
-if dpkg -l | grep linux-image | awk '{print $3}' | grep -q $current_running; then
+if dpkg -l linux-image-amd64 | tail -n 1 | awk '{print $3}' | grep -q $current_running; then
     ok "Linux kernel is up to date." "$DESCRIPTION" "$ENVIRONMENT"
 else
     warning "Linux kernel $current_running is OUTDATED, server restart needed" "$DESCRIPTION" "$ENVIRONMENT"
