d0eb1b126d29e5e618be2d5c2091f04ec8cc98a5
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-05-11 18:54:52 +0200

    Check website success rate once in 5 minutes

---
+++ stages/monitoring/files/plugins/check_http_success_rate.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+source /etc/monitoring/plugins/okfail.sh
+
+test -d /var/lib/httpsuccessrate/ || mkdir -p /var/lib/httpsuccessrate
+
+YESTERDAY=$(date --date "now -24 hours" '+%s')
+
+sqlite3 /var/lib/httpsuccessrate/timeseries.db "CREATE TABLE IF NOT EXISTS ${PLUGIN_NAME} (time timestamp default (strftime('%s', 'now')), status text)"
+
+status=`curl -A "monit-ping-check" -s -o /dev/null -w "%{http_code}" --connect-timeout 20 --max-time 20 $1`
+
+sqlite3 /var/lib/httpsuccessrate/timeseries.db "insert into ${PLUGIN_NAME} (status) values (${status})"
+
+PERCENTAGE=`sqlite3 /var/lib/httpsuccessrate/timeseries.db "select round(100.0 * count(*) / (select count(*) from ${PLUGIN_NAME} where time > ${YESTERDAY})) as percentage from ${PLUGIN_NAME} where status = '200' and time > ${YESTERDAY}"`
+
+if [[ $PERCENTAGE -lt ${WARNING_THRESHOLD} ]]; then
+    warning "The success rate of ${1} is below ${WARNING_THRESHOLD}%"
+elif [[ ${PERCENTAGE} -lt ${CRITICAL_THRESHOLD} ]]; then
+    fail "The success rate of ${1} is below ${CRITICAL_THRESHOLD}%! Check the site health"
+else
+    ok "The success rate of ${1} is ${PERCENTAGE}"
+fi
