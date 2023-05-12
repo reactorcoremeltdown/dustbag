664ab0c904d9d548f6ffb37b0c40383a0794448b
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-05-12 15:22:15 +0200

    Missing space

--- stages/monitoring/files/plugins/check_http_success_rate.sh
+++ stages/monitoring/files/plugins/check_http_success_rate.sh
@@ -7,7 +7,7 @@ test -d /var/lib/httpsuccessrate/ || mkdir -p /var/lib/httpsuccessrate
 YESTERDAY=$(date --date "now -24 hours" '+%s')
 
 COMMAND_STATUS="1"
-while [ ${COMMAND_STATUS} != "0"]; do
+while [ ${COMMAND_STATUS} != "0" ]; do
     sqlite3 /var/lib/httpsuccessrate/timeseries.db "CREATE TABLE IF NOT EXISTS ${PLUGIN_NAME} (time timestamp default (strftime('%s', 'now')), status text)"
     COMMAND_STATUS=$?
 done
@@ -15,7 +15,7 @@ done
 status=`curl -A "monit-ping-check" -s -o /dev/null -w "%{http_code}" --connect-timeout 20 --max-time 20 $1`
 
 COMMAND_STATUS="1"
-while [ ${COMMAND_STATUS} != "0"]; do
+while [ ${COMMAND_STATUS} != "0" ]; do
     sqlite3 /var/lib/httpsuccessrate/timeseries.db "insert into ${PLUGIN_NAME} (status) values (${status})"
     COMMAND_STATUS=$?
 done
@@ -23,7 +23,7 @@ done
 PERCENTAGE="0"
 
 COMMAND_STATUS="1"
-while [ ${COMMAND_STATUS} != "0"]; do
+while [ ${COMMAND_STATUS} != "0" ]; do
     PERCENTAGE=`sqlite3 /var/lib/httpsuccessrate/timeseries.db "select round(100.0 * count(*) / (select count(*) from ${PLUGIN_NAME} where time > ${YESTERDAY})) as percentage from ${PLUGIN_NAME} where status = '200' and time > ${YESTERDAY}" | cut -f 1 -d '.'`
     COMMAND_STATUS=$?
 done
