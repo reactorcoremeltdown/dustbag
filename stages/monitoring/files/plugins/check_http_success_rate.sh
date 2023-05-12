6d755bff712c8aa04a2a8bf2c9df34e263a4a3ef
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-05-12 16:13:06 +0200

    Adjusted sqlite queries

--- stages/monitoring/files/plugins/check_http_success_rate.sh
+++ stages/monitoring/files/plugins/check_http_success_rate.sh
@@ -6,15 +6,27 @@ test -d /var/lib/httpsuccessrate/ || mkdir -p /var/lib/httpsuccessrate
 
 YESTERDAY=$(date --date "now -24 hours" '+%s')
 
-sqlite3 /var/lib/httpsuccessrate/timeseries.db "PRAGMA busy_timeout=3000; CREATE TABLE IF NOT EXISTS ${PLUGIN_NAME} (time timestamp default (strftime('%s', 'now')), status text)"
+sqlite3 /var/lib/httpsuccessrate/timeseries.db <<EOF
+.timeout 3000
+CREATE TABLE IF NOT EXISTS ${PLUGIN_NAME} (time timestamp default (strftime('%s', 'now')), status text);
+EOF
 
 status=`curl -A "monit-ping-check" -s -o /dev/null -w "%{http_code}" --connect-timeout 20 --max-time 20 $1`
 
-sqlite3 /var/lib/httpsuccessrate/timeseries.db "PRAGMA busy_timeout=3000; insert into ${PLUGIN_NAME} (status) values (${status})"
+sqlite3 /var/lib/httpsuccessrate/timeseries.db <<EOF
+.timeout 3000
+insert into ${PLUGIN_NAME} (status) values (${status});
+EOF
 
 PERCENTAGE="0"
 
-PERCENTAGE=`sqlite3 /var/lib/httpsuccessrate/timeseries.db "PRAGMA busy_timeout=3000; select round(100.0 * count(*) / (select count(*) from ${PLUGIN_NAME} where time > ${YESTERDAY})) as percentage from ${PLUGIN_NAME} where status = '200' and time > ${YESTERDAY}" | cut -f 1 -d '.'`
+PERCENTAGE_DEC=$(sqlite3 /var/lib/httpsuccessrate/timeseries.db <<EOF
+.timeout 3000
+select round(100.0 * count(*) / (select count(*) from ${PLUGIN_NAME} where time > ${YESTERDAY})) as percentage from ${PLUGIN_NAME} where status = '200' and time > ${YESTERDAY};
+EOF
+)
+
+PERCENTAGE=`echo "${PERCENTAGE_DEC}" | cut -f 1 -d '.'`
 
 if [[ $PERCENTAGE -lt ${WARNING_THRESHOLD} ]]; then
     warning "The success rate of ${1} is ${PERCENTAGE}%"
