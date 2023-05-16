d86eb2859cca8494aadfded9218565a38004fd58
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-05-16 18:40:07 +0200

    Passing host header to webdav server

--- stages/services/templates/nginx/sites/dav.sh
+++ stages/services/templates/nginx/sites/dav.sh
@@ -76,6 +76,7 @@ server {
         auth_basic_user_file /etc/nginx/htpasswd;
         client_max_body_size 0;
         proxy_pass http://127.0.0.1:38000; 
+        proxy_set_header Host \$http_host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
