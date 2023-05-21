e6701553c78457d41f33b5287e0ab0c57eed4d13
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-05-21 23:46:51 +0200

    Added data script

--- stages/users/files/home/ledger/bin/track_sprint.sh
+++ stages/users/files/home/ledger/bin/track_sprint.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 
+IFS=$'\n'
+
 PASSWORD=`cat /home/ledger/.config/kanboard_secret`
 REQUEST_ID=`date '+%s'`
+INSERTS=""
 
 DATA=`curl -u "jsonrpc:${PASSWORD}" -XPOST -H 'Content-Type: application/json' --data "{ \"jsonrpc\": \"2.0\", \"method\": \"searchTasks\", \"id\": ${REQUEST_ID},\"params\": { \"project_id\": 14, \"query\": \"status:open assignee:RCMD\" }}" https://board.rcmd.space/jsonrpc.php`
 
-echo "$DATA"
+for item in `echo "${DATA}" | jq -cr '.result'`; do
+    TITLE=`echo "${item}" | jq -r '.title'`
+    COLUMN=`echo "${item}" | jq -r '.column_name'`
+    INSERTS=${INSERTS}"\ninsert into sprints(title,column) values(\"${TITLE}\", \"${COLUMN}\");"
+done
+
+echo "${INSERTS}"
+
+# sqlite3 /home/ledger/expenses.db <<EOF
+# BEGIN TRANSACTION;
+# ${INSERTS}
+# COMMIT;
+# EOF
