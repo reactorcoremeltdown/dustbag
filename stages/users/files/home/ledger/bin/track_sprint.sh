d8b8d271d14dfade5bdfe0071e4a80a83069193e
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-05-21 23:59:01 +0200

    Finalized sprint script

--- stages/users/files/home/ledger/bin/track_sprint.sh
+++ stages/users/files/home/ledger/bin/track_sprint.sh
@@ -6,18 +6,18 @@ PASSWORD=`cat /home/ledger/.config/kanboard_secret`
 REQUEST_ID=`date '+%s'`
 INSERTS=""
 
-DATA=`curl -u "jsonrpc:${PASSWORD}" -XPOST -H 'Content-Type: application/json' --data "{ \"jsonrpc\": \"2.0\", \"method\": \"searchTasks\", \"id\": ${REQUEST_ID},\"params\": { \"project_id\": 14, \"query\": \"status:open assignee:RCMD\" }}" https://board.rcmd.space/jsonrpc.php`
+DATA=`curl -s -u "jsonrpc:${PASSWORD}" -XPOST -H 'Content-Type: application/json' --data "{ \"jsonrpc\": \"2.0\", \"method\": \"searchTasks\", \"id\": ${REQUEST_ID},\"params\": { \"project_id\": 14, \"query\": \"status:open assignee:RCMD\" }}" https://board.rcmd.space/jsonrpc.php`
 
-for item in `echo "${DATA}" | jq -cr '.result'`; do
-    TITLE=`echo "${item}" | jq -r '.title'`
+for item in `echo "${DATA}" | jq -cr '.result[]'`; do
+    TASK_ID=`echo "${item}" | jq -r '.id'`
     COLUMN=`echo "${item}" | jq -r '.column_name'`
-    INSERTS=${INSERTS}"\ninsert into sprints(title,column) values(\"${TITLE}\", \"${COLUMN}\");"
+    INSERTS=${INSERTS}"\ninsert into sprints(id,column) values(${TASK_ID}, \"${COLUMN}\");"
 done
 
-echo "${INSERTS}"
+INSERTS_RAW=`echo -e "${INSERTS}"`
 
-# sqlite3 /home/ledger/expenses.db <<EOF
-# BEGIN TRANSACTION;
-# ${INSERTS}
-# COMMIT;
-# EOF
+sqlite3 /home/ledger/expenses.db <<EOF
+BEGIN TRANSACTION;
+${INSERTS_RAW}
+COMMIT;
+EOF
