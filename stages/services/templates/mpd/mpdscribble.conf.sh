3e531a61d69c7d29abb266f23ebbb990d483449b
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-06-04 19:35:52 +0200

    Use two different configs for radioscrobbler and for mpdscribble

--- stages/services/templates/mpd/mpdscribble.conf.sh
+++ stages/services/templates/mpd/mpdscribble.conf.sh
@@ -7,7 +7,7 @@ PASSWORD=$(jq -r '.secrets.lastfm.password' /etc/secrets/secrets.json)
 APIKEY=$(jq -r '.secrets.lastfm.apikey' /etc/secrets/secrets.json)
 APISECRET=$(jq -r '.secrets.lastfm.apisecret' /etc/secrets/secrets.json)
 
-cat <<EOF > /etc/mpdscribble.conf
+cat <<EOF | tee /etc/mpdscribble.conf > /etc/radioscrobbler.conf
 log = syslog
 verbose = 1
 
@@ -16,11 +16,13 @@ url = https://post.audioscrobbler.com/
 username = ${USERNAME}
 password = ${PASSWORD}
 journal = /var/cache/mpdscribble/lastfm.journal
+EOF
 
+cat <<EOF >> /etc/radioscrobbler.conf
 [api]
 key = ${APIKEY}
 secret = ${APISECRET}
 EOF
 
-chmod 640 /etc/mpdscribble.conf
-chown root:mpdscribble /etc/mpdscribble.conf
+chmod 640 /etc/mpdscribble.conf /etc/radioscrobbler.conf
+chown root:mpdscribble /etc/mpdscribble.conf /etc/radioscrobbler.conf
