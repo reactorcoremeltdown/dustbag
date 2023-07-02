acab40c0c029515dd57be60bc807b92ac5326518
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-07-02 17:46:43 +0200

    Most likely webdav has issues running under HTTP/2

--- stages/services/templates/nginx/sites/dav.sh
+++ stages/services/templates/nginx/sites/dav.sh
@@ -52,8 +52,8 @@ server {
 }
 
 server {
-    listen 443 ssl http2;
-    listen [::]:443 ssl http2;
+    listen 443 ssl;
+    listen [::]:443 ssl;
 
     access_log /var/log/nginx/webdav.rcmd.space_access.log json;
     error_log /var/log/nginx/webdav.rcmd.space_error.log;
