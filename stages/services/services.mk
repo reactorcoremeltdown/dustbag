30947c5fab595ba89174d8b5f4939aa59dbe86bd
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-08-01 19:13:14 +0200

    Enabled source file

--- stages/services/services.mk
+++ stages/services/services.mk
@@ -252,6 +252,7 @@ motion:
 misc:
 	install -D -m 755 stages/services/files/usr/local/bin/rcmd-space-stats /usr/local/bin
 	install -D -m 755 stages/services/files/usr/local/bin/kanboard-stats /usr/local/bin
+	install -D -m 755 stages/services/files/usr/local/src/rcmd-functions.mk /usr/local/src
 	@echo "$(ccgreen)Setting up misc scripts completed$(ccend)"
 
 prometheus:
