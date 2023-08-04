ebb0eadb711df6f919b11c057d64ecd462b5583c
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-08-04 14:53:20 +0200

    Using needrestart package to determine the need for service restart

--- stages/monitoring/files/plugins/check_kernel_version.sh
+++ stages/monitoring/files/plugins/check_kernel_version.sh
@@ -2,10 +2,20 @@
 
 source /etc/monitoring/plugins/okfail.sh
 
-current_running=`uname -v | awk '{print $4}'`
+#current_running=`uname -v | awk '{print $4}'`
+#
+#if dpkg -l linux-image-amd64 | tail -n 1 | awk '{print $3}' | grep -q $current_running; then
+#    ok "Linux kernel is up to date." "$DESCRIPTION" "$ENVIRONMENT"
+#else
+#    warning "Linux kernel $current_running is OUTDATED, server restart needed" "$DESCRIPTION" "$ENVIRONMENT"
+#fi
+#
+NEED_RESTART=$(needrestart -k -p)
 
-if dpkg -l linux-image-amd64 | tail -n 1 | awk '{print $3}' | grep -q $current_running; then
-    ok "Linux kernel is up to date." "$DESCRIPTION" "$ENVIRONMENT"
+STATUS=$(echo "${NEED_RESTART}" | awk '{print $1}')
+
+if [[ ${STATUS} != "OK" ]]; then
+    warning "Linux kernel ${NEED_RESTART} is OUTDATED, server restart required" "${DESCRIPTION}" "${ENVIRONMENT}"
 else
-    warning "Linux kernel $current_running is OUTDATED, server restart needed" "$DESCRIPTION" "$ENVIRONMENT"
+    ok "Linux kernel is up to date." "${DESCRIPTION}" "${ENVIRONMENT}"
 fi
