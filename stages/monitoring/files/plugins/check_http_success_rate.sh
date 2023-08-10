3ca36affd4779a5a8fbb1a057873b8a21c509424
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-08-10 15:57:29 +0200

    Increased the timeout value

--- stages/monitoring/files/plugins/check_http_success_rate.sh
+++ stages/monitoring/files/plugins/check_http_success_rate.sh
@@ -5,36 +5,37 @@ source /etc/monitoring/plugins/okfail.sh
 test -d /var/lib/httpsuccessrate/ || mkdir -p /var/lib/httpsuccessrate
 
 TIMEFRAME=$(date --date "now -3 hours" '+%s')
+TIMEOUT="10000"
 
 TOTAL_COUNT=$(sqlite3 /var/lib/httpsuccessrate/timeseries.db <<EOF
-.timeout 3000
+.timeout ${TIMEOUT}
 select count(*) from ${PLUGIN_NAME};
 EOF
 )
 
 if [[ ${TOTAL_COUNT} -gt 1000 ]]; then
     sqlite3 /var/lib/httpsuccessrate/timeseries.db <<EOF
-.timeout 3000
+.timeout ${TIMEOUT}
 delete from ${PLUGIN_NAME} where time < strftime('%s', 'now', '-2 days');
 EOF
 fi
 
 sqlite3 /var/lib/httpsuccessrate/timeseries.db <<EOF
-.timeout 3000
+.timeout ${TIMEOUT}
 CREATE TABLE IF NOT EXISTS ${PLUGIN_NAME} (time timestamp default (strftime('%s', 'now')), status text);
 EOF
 
 status=`curl -A "monit-ping-check" -s -o /dev/null -w "%{http_code}" --connect-timeout 20 --max-time 20 $1`
 
 sqlite3 /var/lib/httpsuccessrate/timeseries.db <<EOF
-.timeout 3000
+.timeout ${TIMEOUT}
 insert into ${PLUGIN_NAME} (status) values (${status});
 EOF
 
 PERCENTAGE="0"
 
 PERCENTAGE_DEC=$(sqlite3 /var/lib/httpsuccessrate/timeseries.db <<EOF
-.timeout 3000
+.timeout ${TIMEOUT}
 select round(100.0 * count(*) / (select count(*) from ${PLUGIN_NAME} where time > ${TIMEFRAME})) as percentage from ${PLUGIN_NAME} where status = '200' and time > ${TIMEFRAME};
 EOF
 )
