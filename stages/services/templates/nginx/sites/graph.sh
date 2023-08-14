a095b7e9c247e055a27a8c97a375df2e53792c59
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-08-14 17:54:48 +0200

    Allowing to render without requesting a client certificate

--- stages/services/templates/nginx/sites/graph.sh
+++ stages/services/templates/nginx/sites/graph.sh
@@ -75,6 +75,29 @@ server {
         proxy_send_timeout      7m;
         proxy_read_timeout      7m;
     }
+    location /render {
+        # auth_basic "Protected area";
+        # auth_basic_user_file /etc/nginx/htpasswd;
+        proxy_pass http://127.0.0.1:27105;
+        proxy_http_version 1.1;
+
+        # Ensuring it can use websockets
+        proxy_set_header   Upgrade \$http_upgrade;
+        proxy_set_header   Connection "upgrade";
+        proxy_set_header   X-Real-IP \$remote_addr;
+        proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto http;
+        proxy_redirect     http:// \$scheme://;
+
+        # The proxy must preserve the host because gotify verifies the host with the origin
+        # for WebSocket connections
+        proxy_set_header   Host \$http_host;
+
+        # These sets the timeout so that the websocket can stay alive
+        proxy_connect_timeout   7m;
+        proxy_send_timeout      7m;
+        proxy_read_timeout      7m;
+    }
 }
 
 EOF
