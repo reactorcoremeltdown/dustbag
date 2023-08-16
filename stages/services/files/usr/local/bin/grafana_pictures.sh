d3dcf4ff947bae70cba94ba54eb003338649d8d7
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-08-16 14:42:10 +0200

    Updated export location

--- stages/services/files/usr/local/bin/grafana_pictures.sh
+++ stages/services/files/usr/local/bin/grafana_pictures.sh
@@ -6,5 +6,5 @@ for i in `jq -c '.secrets.grafana_pictures[]' /etc/secrets/secrets.json`; do
 	NAME=`echo "${i}" | jq -r .name`
 	URL=`echo "${i}" | jq -r .url`
 
-	curl -s "${URL}" > /var/storage/wastebox/tiredsysadmin.cc/wiki/pictures/static/${NAME}.png
+	curl -s "${URL}" > /opt/apps/graphs/${NAME}.png
 done
