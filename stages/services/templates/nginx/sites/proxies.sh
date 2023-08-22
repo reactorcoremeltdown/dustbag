3f4ea5ec3839088d8f061687d246d63b204c9642
Azer Abdullaev <azer.abdullaev.berlin+git@gmail.com>
2023-08-22 11:39:07 +0200

    Updated proxy config for an RSS feed

--- stages/services/templates/nginx/sites/proxies.sh
+++ stages/services/templates/nginx/sites/proxies.sh
@@ -11,6 +11,7 @@ server {
     error_log /var/log/nginx/proxies.rcmd.space_error.log;
 
     location / {
+        proxy_ssl_server_name on;
         proxy_pass https://mezha.media:443;
         proxy_set_header Host mezha.media;
         proxy_set_header User-Agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
