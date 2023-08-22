323b4554c9cc7bce0dc554d0dbe49a7d7b67bd2c
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-08-23 00:11:00 +0200

    Adjusted lock script

--- stages/lock/files/lock.sh
+++ stages/lock/files/lock.sh
@@ -2,11 +2,12 @@
 
 while true; do
     if test -f /var/lock/drone.lock; then
-        echo "Another instance of a pipeline is running on this machine"
+        echo "Another instance of a pipeline is running on this machine, pausing execution for 1 minute"
     else
+        echo "Pipeline unlocked!"
         break;
     fi
-    sleep 5
+    sleep 60
 done
 
 touch /var/lock/drone.lock
