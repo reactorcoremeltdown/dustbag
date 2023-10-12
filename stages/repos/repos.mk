8e3f5d3d474eae0aa8123749cf6b0266a372f4e2
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-10-12 12:45:20 +0200

    Fixed gpg key installation

--- stages/repos/repos.mk
+++ stages/repos/repos.mk
@@ -2,7 +2,6 @@ repos: setup_debian_repositories update_debian_repositories
 	@echo "$(ccgreen)Setting up Debian repositories completed$(ccend)"
 
 setup_debian_repositories:
-	test -f /usr/share/keyrings/elasticstack.gpg && rm -f /usr/share/keyrings/elasticstack.gpg
 	bash stages/repos/templates/repo.sh stages/repos/variables/repos.json
 
 update_debian_repositories:
