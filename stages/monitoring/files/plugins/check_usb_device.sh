114a752565c32ccc18bfefb291868140781cf6c7
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-11-16 13:44:58 +0100

    Added relay checks for home printer usb device

---
+++ stages/monitoring/files/plugins/check_usb_device.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+source /etc/monitoring/plugins/okfail.sh
+
+DEVICE=$(lsusb -d ${1})
+ret_code=$?
+
+if [[ $ret_code == 0 ]]; then
+    ok "USB device online: ${DEVICE}" "$DESCRIPTION"
+else
+    fail "USB device offline" "$DESCRIPTION"
+fi
