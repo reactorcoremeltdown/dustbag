c41e936a94cda126c2dc5ddf137d6f100a379cd4
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-12-20 22:50:12 +0100

    Added an RSS proxy for Reddit

--- stages/services/templates/nginx/sites/proxies.sh
+++ stages/services/templates/nginx/sites/proxies.sh
@@ -17,6 +17,20 @@ server {
         proxy_set_header User-Agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
     }
 }
+server {
+    listen 80;
+    server_name reddit-proxy.reader.tiredsysadmin.cc;
+
+    access_log /var/log/nginx/proxies.rcmd.space_access.log json;
+    error_log /var/log/nginx/proxies.rcmd.space_error.log;
+
+    location / {
+        proxy_ssl_server_name on;
+        proxy_pass https://www.reddit.com:443;
+        proxy_set_header Host www.reddit.com;
+        proxy_set_header User-Agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
+    }
+}
 EOF
 
 ln -sf /etc/nginx/sites-available/proxies.conf /etc/nginx/sites-enabled/proxies.conf
