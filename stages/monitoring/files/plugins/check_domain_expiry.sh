3360aa36d490f28be3da702d03307db8602a7d58
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-12-27 10:11:47 +0100

    Added domain expiry monitoring

---
+++ stages/monitoring/files/plugins/check_domain_expiry.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+source /etc/monitoring/plugins/okfail.sh
+
+EXPIRY_DATE=`whois "${1}" | grep 'Registry Expiry Date' | cut -f 2- -d ':'`
+
+TIMESTAMP=`date --date "${EXPIRY_DATE}" '+%s'`
+CURRENT_TIMESTAMP=`date '+%s'`
+
+DELTA=`echo "(${TIMESTAMP} - ${CURRENT_TIMESTAMP})/86400"`
+
+if test "${DELTA}" -lt 6; then
+    warning "The domain ${1} expires in ${DELTA} days.\\nGo to https://namecheap.com to verify renewal status."
+elif test "${DELTA}" -lt 3; then
+    fail "The domain ${1} expires in ${DELTA} days!\\nGo to https://namecheap.com ASAP to fix renewal process!"
+else
+    ok "The domain ${1} expires in ${DELTA} days."
+fi
