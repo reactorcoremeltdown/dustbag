b6576130aeef31a8893ce6af5930bf35243073c6
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2023-12-29 12:41:57 +0100

    Separated project_id into a variable

--- stages/users/files/home/ledger/bin/track_sprint.sh
+++ stages/users/files/home/ledger/bin/track_sprint.sh
@@ -5,14 +5,15 @@ IFS=$'\n'
 PASSWORD=`cat /home/ledger/.config/kanboard_secret`
 REQUEST_ID=`date '+%s'`
 INSERTS=""
+SPRINT_PROJECT_ID="14"
 
-DATA=`curl -s -u "jsonrpc:${PASSWORD}" -XPOST -H 'Content-Type: application/json' --data "{ \"jsonrpc\": \"2.0\", \"method\": \"searchTasks\", \"id\": ${REQUEST_ID},\"params\": { \"project_id\": 14, \"query\": \"status:open assignee:RCMD\" }}" https://board.rcmd.space/jsonrpc.php`
+DATA=`curl -s -u "jsonrpc:${PASSWORD}" -XPOST -H 'Content-Type: application/json' --data "{ \"jsonrpc\": \"2.0\", \"method\": \"searchTasks\", \"id\": ${REQUEST_ID},\"params\": { \"project_id\": ${SPRINT_PROJECT_ID}, \"query\": \"status:open assignee:RCMD\" }}" https://board.rcmd.space/jsonrpc.php`
 
 sleep 1
 
 COLUMNS_REQ_ID=`date +%s`
 
-COLUMNS=`curl -s -u "jsonrpc:${PASSWORD}" -XPOST -H 'Content-Type: application/json' --data "{\"jsonrpc\": \"2.0\", \"method\": \"getColumns\", \"id\": ${COLUMNS_REQ_ID}, \"params\": [ 14 ] }" https://board.rcmd.space/jsonrpc.php`
+COLUMNS=`curl -s -u "jsonrpc:${PASSWORD}" -XPOST -H 'Content-Type: application/json' --data "{\"jsonrpc\": \"2.0\", \"method\": \"getColumns\", \"id\": ${COLUMNS_REQ_ID}, \"params\": [ ${SPRINT_PROJECT_ID} ] }" https://board.rcmd.space/jsonrpc.php`
 
 echo "${COLUMNS}"
 
