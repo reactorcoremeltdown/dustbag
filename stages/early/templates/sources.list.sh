1aa732c80c921ae5c724a0ad9422e77342d6f4b2
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-01-05 17:35:20 +0100

    Fixed HEREDOC

--- stages/early/templates/sources.list.sh
+++ stages/early/templates/sources.list.sh
@@ -7,18 +7,18 @@ MIRROR="https://deb.debian.org"
 
 if ![ -f "/usr/bin/raspi-config" ]; then
     cat <<EOF > /etc/apt/sources.list
-    deb ${MIRROR}/debian ${DISTRO} main
-    deb-src ${MIRROR}/debian ${DISTRO} main
-    deb ${MIRROR}/debian ${DISTRO}-updates main
-    deb-src ${MIRROR}/debian ${DISTRO}-updates main
-    deb ${MIRROR}/debian ${DISTRO}-backports main
-    deb-src ${MIRROR}/debian ${DISTRO}-backports main
-    EOF
+deb ${MIRROR}/debian ${DISTRO} main
+deb-src ${MIRROR}/debian ${DISTRO} main
+deb ${MIRROR}/debian ${DISTRO}-updates main
+deb-src ${MIRROR}/debian ${DISTRO}-updates main
+deb ${MIRROR}/debian ${DISTRO}-backports main
+deb-src ${MIRROR}/debian ${DISTRO}-backports main
+EOF
+fi
 
-    if [[ ${DEBIAN_VERSION} = '11' ]]; then
-        cat <<EOF >> /etc/apt/sources.list
-    deb http://security.debian.org/debian-security ${DISTRO}-security main
-    deb-src http://security.debian.org/debian-security ${DISTRO}-security main
-    EOF
-    fi
+if [[ ${DEBIAN_VERSION} = '11' ]]; then
+    cat <<EOF >> /etc/apt/sources.list
+deb http://security.debian.org/debian-security ${DISTRO}-security main
+deb-src http://security.debian.org/debian-security ${DISTRO}-security main
+EOF
 fi
