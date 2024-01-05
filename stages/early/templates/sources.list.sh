4ceb1f2752a1f8dae68ea2fd4d08d9446af75236
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-01-05 16:00:21 +0100

    DO NOT provision custom debian repos on Raspberry!

--- stages/early/templates/sources.list.sh
+++ stages/early/templates/sources.list.sh
@@ -5,18 +5,20 @@ DISTRO=`lsb_release -cs`
 DEBIAN_VERSION=`lsb_release -sr`
 MIRROR="https://deb.debian.org"
 
-cat <<EOF > /etc/apt/sources.list
-deb ${MIRROR}/debian ${DISTRO} main
-deb-src ${MIRROR}/debian ${DISTRO} main
-deb ${MIRROR}/debian ${DISTRO}-updates main
-deb-src ${MIRROR}/debian ${DISTRO}-updates main
-deb ${MIRROR}/debian ${DISTRO}-backports main
-deb-src ${MIRROR}/debian ${DISTRO}-backports main
-EOF
+if ![ -f "/usr/bin/raspi-config" ]; then
+    cat <<EOF > /etc/apt/sources.list
+    deb ${MIRROR}/debian ${DISTRO} main
+    deb-src ${MIRROR}/debian ${DISTRO} main
+    deb ${MIRROR}/debian ${DISTRO}-updates main
+    deb-src ${MIRROR}/debian ${DISTRO}-updates main
+    deb ${MIRROR}/debian ${DISTRO}-backports main
+    deb-src ${MIRROR}/debian ${DISTRO}-backports main
+    EOF
 
-if [[ ${DEBIAN_VERSION} = '11' ]]; then
-    cat <<EOF >> /etc/apt/sources.list
-deb http://security.debian.org/debian-security ${DISTRO}-security main
-deb-src http://security.debian.org/debian-security ${DISTRO}-security main
-EOF
+    if [[ ${DEBIAN_VERSION} = '11' ]]; then
+        cat <<EOF >> /etc/apt/sources.list
+    deb http://security.debian.org/debian-security ${DISTRO}-security main
+    deb-src http://security.debian.org/debian-security ${DISTRO}-security main
+    EOF
+    fi
 fi
