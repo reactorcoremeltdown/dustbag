ad6dcc5af047b32c5b8547bf513a1c5c23606946
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-01-07 00:59:28 +0100

    Getting davfs2 accounts from vault

--- stages/services/includes/media/templates/davfs2/secrets.sh
+++ stages/services/includes/media/templates/davfs2/secrets.sh
@@ -4,8 +4,10 @@ set -e
 
 IFS=$'\n'
 
+ACCOUNTS=$(vault-request-key accounts system/davfs2)
+
 echo "## Davfs2 Secrets, managed by dustbag" > /etc/davfs2/secrets
-for i in $(jq -cr '.secrets.davfs2.accounts[]' /etc/secrets/secrets.json); do
+for i in $(echo "${ACCOUNTS}" | yq -cr '.[]'); do
     source <(echo "${i}" | jq -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     echo "${mountpoint} ${username} ${password}" >> /etc/davfs2/secrets
 done
