1cddf688b6a66f1be5f1aa8cc0dee62d24902a08
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-02-07 11:06:27 +0100

    Diskplayer has been moved to its own repo

--- stages/services/services.mk
+++ stages/services/services.mk
@@ -20,7 +20,7 @@ CRONS := stages/services/files/crons/fermium
 DEVICEPING_ID := deviceping_fermium
 ROLE := homeserver
 
-services: users packages crons nginx_proxies nginx tinc_client mpd diskplayer motion bootconfig deviceping drone_runner_arm pki vault_seal
+services: users packages crons nginx_proxies nginx tinc_client mpd motion bootconfig deviceping drone_runner_arm pki vault_seal
 	@echo "$(ccgreen)Setting up services completed$(ccend)"
 
 ## Seedbox
