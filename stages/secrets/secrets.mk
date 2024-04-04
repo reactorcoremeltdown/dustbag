559fc261467e4e2aa1388446329dee803dd57616
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-04-04 20:26:21 +0200

    Changed mode & permissions for file based secrets

--- stages/secrets/secrets.mk
+++ stages/secrets/secrets.mk
@@ -2,8 +2,10 @@ secrets: directory unpack
 	@echo "$(ccgreen)Setting up secrets completed$(ccend)"
 
 directory:
-	install -d -m 500 /etc/secrets
+	groupadd secrets
+	install -d -m 550 /etc/secrets
+	chown -R root:secrets /etc/secrets
 
 unpack: directory
 	stages/secrets/files/bin/age-$(shell uname -s)-$(shell uname -m) -d stages/secrets/files/etc/secrets/secrets.json.age > /etc/secrets/secrets.json
-	chmod 400 /etc/secrets/secrets.json
+	chmod 440 /etc/secrets/secrets.json
