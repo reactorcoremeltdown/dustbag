dcafb60f343186f08e1a8ae9cd276b3a50172227
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-04-18 23:15:05 +0200

    Added a function to reset gmail config

--- stages/secrets/secrets.mk
+++ stages/secrets/secrets.mk
@@ -1,4 +1,4 @@
-secrets: directory unpack
+secrets: directory unpack reset_gmail
 	@echo "$(ccgreen)Setting up secrets completed$(ccend)"
 
 directory:
@@ -9,3 +9,9 @@ directory:
 unpack: directory
 	stages/secrets/files/bin/age-$(shell uname -s)-$(shell uname -m) -d stages/secrets/files/etc/secrets/secrets.json.age > /etc/secrets/secrets.json
 	chmod 440 /etc/secrets/secrets.json
+
+remove_config_gmail:
+	rm -fr /etc/secrets/gmail_config
+
+reset_gmail: remove_config_gmail misc
+	@echo "$(ccgreen) GMail config has been updated"
