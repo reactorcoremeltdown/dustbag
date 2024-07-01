1642344474d831256548e9ebba7dce760909b1eb
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-07-01 12:12:11 +0200

    Fixed typos

--- stages/monitoring/files/plugins/check_feature_toggle.sh
+++ stages/monitoring/files/plugins/check_feature_toggle.sh
@@ -7,18 +7,18 @@ source ${PLUGINSDIR}/okfail.sh
 
 API_TOKEN=`jq -cr '.secrets.monitoring.token' /etc/secrets/secrets.json`
 
-ONETIME_TOKEN=`curl -XPOST -H "${UA}" --data--urlencode "token=${API_TOKEN}" https://api.rcmd.space/v5/token/get`
-JOB_ID=`curl -XPOST -H "${UA}" \
+ONETIME_TOKEN=`curl -s -XPOST -H "${UA}" --data-urlencode "token=${API_TOKEN}" https://api.rcmd.space/v6/token/get`
+JOB_ID=`curl -s -XPOST -H "${UA}" \
     --data-urlencode "token=${ONETIME_TOKEN}" \
-    --data-urlencode="queue=ft_${1}" \
-    https://api.rcmd.space/v5/queue/get-job`
+    --data-urlencode "queue=ft_${1}" \
+    https://api.rcmd.space/v6/queue/get-job`
 
-ONETIME_TOKEN=`curl -XPOST -H "${UA}" --data--urlencode "token=${API_TOKEN}" https://api.rcmd.space/v5/token/get`
-STATUS=`curl -XPOST -H "${UA}" \
+ONETIME_TOKEN=`curl -s -XPOST -H "${UA}" --data-urlencode "token=${API_TOKEN}" https://api.rcmd.space/v6/token/get`
+STATUS=`curl -s -XPOST -H "${UA}" \
     --data-urlencode "job=${JOB_ID}" \
     --data-urlencode "token=${ONETIME_TOKEN}" \
-    --data-urlencode="queue=ft_${1}" \
-    https://api.rcmd.space/v5/queue/get-job-payload`
+    --data-urlencode "queue=ft_${1}" \
+    https://api.rcmd.space/v6/queue/get-job-payload`
 
 if [[ ${STATUS} = "1" ]]; then
     ok "Feature ${1} is enabled"
