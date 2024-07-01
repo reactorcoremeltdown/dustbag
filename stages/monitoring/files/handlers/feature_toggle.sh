0ec37d5fdc0d836d76790f88f4d98497fff9f7b0
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-07-01 11:51:46 +0200

    Small fixes for ft notifier

--- stages/monitoring/files/handlers/feature_toggle.sh
+++ stages/monitoring/files/handlers/feature_toggle.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 case $NAME in
-  "motion")
-    if [[ ${STATUS} = '1' ]]; then
+  "ft_motion")
+    if [[ ${STATUS} = '0' ]]; then
       systemctl enable motion.service && systemctl start motion.service
     else
       systemctl disable motion.service && systemctl stop motion.service
