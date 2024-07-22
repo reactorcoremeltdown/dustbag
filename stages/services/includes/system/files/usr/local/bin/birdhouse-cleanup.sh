02a8bdd9bbe2de1014724aa46166ed53addaa337
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-07-22 10:19:07 +0200

    Raised percentage to 15

--- stages/services/includes/system/files/usr/local/bin/birdhouse-cleanup.sh
+++ stages/services/includes/system/files/usr/local/bin/birdhouse-cleanup.sh
@@ -4,6 +4,6 @@ STORAGE_PATH="/media/external"
 
 USED=`df --output=pcent ${STORAGE_PATH} | tail -n 1 | sed 's| ||g' | cut -f 1 -d %`
 
-if [[ ${USED} -lt 5 ]]; then
+if [[ ${USED} -lt 15 ]]; then
     rm -fr ${STORAGE_PATH}/*mkv
 fi
