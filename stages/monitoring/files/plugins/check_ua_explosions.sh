ddcf4b5759b75d1700da15c93c40211b78841535
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-12-10 15:03:31 +0100

    Comparing string literals

--- stages/monitoring/files/plugins/check_ua_explosions.sh
+++ stages/monitoring/files/plugins/check_ua_explosions.sh
@@ -28,7 +28,7 @@ esac
 
 if [[ ${CURRENT_SCORE} -gt ${PREVIOUS_SCORE} ]]; then
         fail "The number of explosions at location ${LOCATION} has increased to ${CURRENT_SCORE}\\n\\nCheck reports here:\\n- https://alerts.in.ua/en\\n- https://t.me/ivan_fedorov_zp\\n- https://t.me/vanek_nikolaev\\n"
-elif [[ ${CURRENT_SCORE} -gt 0 ]]; then
+elif [[ ${CURRENT_SCORE} != '0' ]]; then
         warning "There are no new reported explosions at location ${LOCATION}\\nThe current number of cases is ${CURRENT_SCORE}"
 else
         ok "There are no reported explosions today at location ${LOCATION}"
