bd46cdb5f484b5a15f7c6d03d82d4904aa903ec4
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2024-12-20 13:45:33 +0100

    Extended podcasts domains

--- stages/services/includes/nginx/templates/nginx/sites/podcasts.sh
+++ stages/services/includes/nginx/templates/nginx/sites/podcasts.sh
@@ -62,6 +62,66 @@ server {
         try_files \$uri \$uri/ =404;
     }
 }
+
+server {
+    listen 80;
+    listen [::]:80;
+    server_name podcasts.tiredsysadmin.cc;
+
+    return 301 https://\$server_name\$request_uri;
+}
+
+server {
+    listen 443 ssl http2;
+    listen [::]:443 ssl http2;
+
+    access_log /var/log/nginx/podcasts.tiredsysadmin.cc_access.log json;
+    error_log /var/log/nginx/podcasts.tiredsysadmin.cc_error.log;
+
+    ### SSL cert files ###
+    ssl_certificate ${blog_ssl_certificate};
+    ssl_certificate_key ${blog_ssl_certificate_key};
+
+    ### Add SSL specific settings here ###
+    ssl_session_timeout 10m;
+
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers '${ssl_ciphers}';
+    ssl_prefer_server_ciphers on;
+
+    ### Compression
+    gzip on;
+    gzip_comp_level    5;
+    gzip_min_length    256;
+    gzip_proxied       any;
+    gzip_vary          on;
+    gzip_types
+    application/rss+xml
+    text/css;
+
+
+    server_name podcasts.tiredsysadmin.cc;
+
+    location / {
+        proxy_pass http://10.8.0.102:80;
+        proxy_http_version 1.1;
+        client_max_body_size 0;
+
+        proxy_set_header   Host \$http_host;
+    }
+
+    location /mds.xml {
+        root /var/storage/wastebox/tiredsysadmin.cc/podcasts;
+        charset UTF-8;
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /mds {
+        root /var/storage/wastebox/tiredsysadmin.cc/podcasts;
+        charset UTF-8;
+        try_files \$uri \$uri/ =404;
+    }
+}
 EOF
 
 ln -sf /etc/nginx/sites-available/podcasts.conf /etc/nginx/sites-enabled/podcasts.conf
