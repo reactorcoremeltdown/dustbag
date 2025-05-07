1d177b497819fa23027a5920761df4571d05924b
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2025-05-07 18:02:41 +0200

    Escaping the pipe symbol

--- stages/monitoring/files/handlers/telegram.sh
+++ stages/monitoring/files/handlers/telegram.sh
@@ -5,7 +5,7 @@ chat_id=`cat /etc/secrets/secrets.json | jq -r .secrets.telegram.chat_id`
 
 text=""
 tail="\\n**Hostname**: $(echo ${HOSTNAME} | sed 's/\./\\\\./g')\\n**Check name**: $NAME\\n**Description**: $(echo "${MESSAGE}" | sed 's/\./\\\\./g;s/!/\\\\!/g')"
-tail_extended="\\n**Hostname**: $(echo ${HOSTNAME} | sed 's/\./\\\\./g')\\n**Check name**: $NAME\\n**Description**: $(echo "${MESSAGE}" | sed 's/\./\\\\./g;s/!/\\\\!/g')\\n\\n[Login](https://http2ssh.tiredsysadmin.cc/go.html?ssh=ssh://rcmd@$(echo ${HOSTNAME} | sed 's/\./\\\\./g')) | [Downtime 1h](https://api.rcmd.space/internal/protected/downtime?check=${NAME}&downtime=1h) | [Downtime 24h](https://api.rcmd.space/internal/protected/downtime?check=${NAME}&downtime=24h)"
+tail_extended="\\n**Hostname**: $(echo ${HOSTNAME} | sed 's/\./\\\\./g')\\n**Check name**: $NAME\\n**Description**: $(echo "${MESSAGE}" | sed 's/\./\\\\./g;s/!/\\\\!/g')\\n\\n[Login](https://http2ssh.tiredsysadmin.cc/go.html?ssh=ssh://rcmd@$(echo ${HOSTNAME} | sed 's/\./\\\\./g')) \\\\| [Downtime 1h](https://api.rcmd.space/internal/protected/downtime?check=${NAME}&downtime=1h) \\\\| [Downtime 24h](https://api.rcmd.space/internal/protected/downtime?check=${NAME}&downtime=24h)"
 
 case $STATUS in
   "0")
