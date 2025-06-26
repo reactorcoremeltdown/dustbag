13f0765f528ffebe679a4565afd4eb5dbbf5a809
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2025-06-26 15:02:05 +0200

    Install golang everywhere

--- stages/packages/packages.mk
+++ stages/packages/packages.mk
@@ -12,9 +12,13 @@ else
 	PYTHON_PACKAGES := $(shell yq -r '.python.noop[]' stages/packages/variables/packages.yaml | xargs)
 endif
 
-packages: repos
+packages: repos inplace_packages_fix
 	dpkg-query -s $(PACKAGES) > /dev/null || DEBIAN_FRONTEND=noninteractive apt-get -o Acquire::ForceIPv4=true install -y $(PACKAGES)
 	mkdir -p /opt/virtualenv || true
 	python3 -m venv /opt/virtualenv
 	/opt/virtualenv/bin/pip3 install $(PYTHON_PACKAGES) || true
 	@echo "$(ccgreen)Installing Packages completed$(ccend)"
+
+inplace_packages_fix:
+	apt-get -o Acquire::ForceIPv4=true install -y golang-go
+	apt-get -y purge yq
