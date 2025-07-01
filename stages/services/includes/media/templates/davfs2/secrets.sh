d1475bf27111b94805bbf93f81d8ba5b2f41edbc
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2025-07-01 16:32:59 +0200

    Iterate over davfs secrets as json slices

--- stages/services/includes/media/templates/davfs2/secrets.sh
+++ stages/services/includes/media/templates/davfs2/secrets.sh
@@ -7,7 +7,7 @@ IFS=$'\n'
 ACCOUNTS=$(vault-request-key accounts system/davfs2)
 
 echo "## Davfs2 Secrets, managed by dustbag" > /etc/davfs2/secrets
-for i in $(echo "${ACCOUNTS}" | yq -cr '.[]'); do
+for i in $(echo "${ACCOUNTS}" | yq -o=json -I=0 -r '.[]'); do
     source <(echo "${i}" | jq -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     echo "${mountpoint} ${username} ${password}" >> /etc/davfs2/secrets
 done
