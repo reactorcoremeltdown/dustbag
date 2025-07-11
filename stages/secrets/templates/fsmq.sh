14cd2f7c891086e96ce104cc9844ab16037d2713
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2025-07-11 12:08:22 +0200

    Fixed another typo

--- stages/secrets/templates/fsmq.sh
+++ stages/secrets/templates/fsmq.sh
@@ -54,7 +54,7 @@ EOF
     echo "${YAML}" | podman secret create rcmd-api-v6 -
     systemctl start rcmd-api-v6.service
 
-    kubect get namespace api || kubectl create namespace api
+    kubectl get namespace api || kubectl create namespace api
     kubectl delete secret --namespace=api rcmd-api-v6 || true
     echo "${YAML}" | kubectl create secret generic --namespace=api rcmd-api-v6 --from-file=fsmq.yaml=/dev/stdin
 fi
