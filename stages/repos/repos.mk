de9b951c6e37e02b75c12d35a9702c68651674b4
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2025-10-02 12:15:35 +0200

    Added reprepro configs to production machine

--- stages/repos/repos.mk
+++ stages/repos/repos.mk
@@ -1,8 +1,17 @@
+ifeq ($(MAKECMDGOALS), production)
+repos: setup_debian_repositories update_debian_repositories reprepro
+	@echo "$(ccgreen)Setting up Debian repositories completed$(ccend)"
+else
 repos: setup_debian_repositories update_debian_repositories
 	@echo "$(ccgreen)Setting up Debian repositories completed$(ccend)"
+endif
 
 setup_debian_repositories:
 	bash stages/repos/templates/repo.sh stages/repos/variables/repos.yaml
 
 update_debian_repositories:
 	apt -y update
+
+reprepro:
+	apt -y install reprepro
+	bash stages/repos/templates/reprepro.sh stages/repos/variables/repos.yaml
---
+++ stages/repos/templates/reprepro.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+IFS=$'\n'
+
+for branch in `yq -o=json -I=0 '.debian.reprepro.branches[]' ${1}`; do
+    location=$(echo "${branch}" | jq -cr '.location')
+    test -d "${location}/repo/conf" || mkdir -p "${location}/repo/conf"
+    name=$(echo "${branch}" | jq -cr '.name')
+    origin=$(echo "${branch}" | jq -cr '.origin')
+    limit=$(echo "${branch}" | jq -cr '.limit')
+    components=$(echo "${branch}" | jq -cr '.components')
+    description=$(echo "${branch}" | jq -cr '.description')
+    signwith=$(echo "${branch}" | jq -cr '.signwith')
+
+    DESTINATION="${location}/repo/conf/distributions-test"
+
+    test -f ${DESTINATION} && cat /dev/null > ${DESTINATION}
+
+    for distribution in `echo "${branch}" | jq -cr '.distributions[]'`; do
+        distro=$(echo "${distribution}" | jq -cr '.name')
+        architectures=$(echo "${distribution}" | jq -cr '.architectures')
+
+        echo "Origin: ${origin}" >> ${DESTINATION}
+        echo "Label: ${origin}" >> ${DESTINATION}
+        echo "Codename: ${distro}" >> ${DESTINATION}
+        echo "Architectures: ${architectures}" >> ${DESTINATION}
+        echo "Limit: ${limit}" >> ${DESTINATION}
+        echo "Components: ${components}" >> ${DESTINATION}
+        echo "Description: ${description}" >> ${DESTINATION}
+        echo "SignWith: ${signwith}" >> ${DESTINATION}
+        echo "" >> ${DESTINATION}
+    done
+done
