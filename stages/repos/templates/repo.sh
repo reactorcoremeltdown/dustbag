cfd0dc6376f626a9cf4ab61246593fd474c00f0c
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2025-12-10 16:55:52 +0100

    Fixed repo template

--- stages/repos/templates/repo.sh
+++ stages/repos/templates/repo.sh
@@ -12,16 +12,20 @@ for repo in `yq -o=json -I=0 '.debian.repositories[]' ${1}`; do
     unset distro
     source <(echo "${repo}" | jq  -cr '. | to_entries[] | [.key,(.value|@sh)] | join("=")')
     if echo "${install_on}" | grep -oq ${MAKECMDGOALS}; then
-        echo "Current distro is ${distro}"
-        test -z ${distro} && distro=${DISTRO_SLUG}
-        if [[ ${DEBIAN_VERSION} -lt 11 ]]; then
-            KEYRING="/etc/apt/trusted.gpg.d/${name}.gpg"
-            test -z ${key} || test -f ${KEYRING} || apt-key --keyring ${KEYRING} adv --fetch-keys --no-tty ${key}
-            echo "${repo}" | jq -cr '. | "deb \(.url) DISTRO \(.section)"' | sed "s|DISTRO|${distro}|g" > /etc/apt/sources.list.d/${name}.list
+        if echo "${state}" || grep -oq "present"; then
+            echo "Current distro is ${distro}"
+            test -z ${distro} && distro=${DISTRO_SLUG}
+            if [[ ${DEBIAN_VERSION} -lt 11 ]]; then
+                KEYRING="/etc/apt/trusted.gpg.d/${name}.gpg"
+                test -z ${key} || test -f ${KEYRING} || apt-key --keyring ${KEYRING} adv --fetch-keys --no-tty ${key}
+                echo "${repo}" | jq -cr '. | "deb \(.url) DISTRO \(.section)"' | sed "s|DISTRO|${distro}|g" > /etc/apt/sources.list.d/${name}.list
+            else
+                KEYRING="/usr/share/keyrings/${name}.gpg"
+                test -z ${key} || test -f ${KEYRING} || curl -s -L ${key} | gpg --dearmor > ${KEYRING}
+                echo "${repo}" | jq -cr '. | "deb [signed-by=KEYRING] \(.url) DISTRO \(.section)"' | sed "s|DISTRO|${distro}|g;s|KEYRING|${KEYRING}|" > /etc/apt/sources.list.d/${name}.list
+            fi
         else
-            KEYRING="/usr/share/keyrings/${name}.gpg"
-            test -z ${key} || test -f ${KEYRING} || curl -s -L ${key} | gpg --dearmor > ${KEYRING}
-            echo "${repo}" | jq -cr '. | "deb [signed-by=KEYRING] \(.url) DISTRO \(.section)"' | sed "s|DISTRO|${distro}|g;s|KEYRING|${KEYRING}|" > /etc/apt/sources.list.d/${name}.list
+            rm -fv /etc/apt/sources.list.d/${name}.list || true
         fi
     else
         rm -fv /etc/apt/sources.list.d/${name}.list || true
