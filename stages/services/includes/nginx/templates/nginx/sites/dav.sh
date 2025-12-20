00b375f87c4d60db52c9814eca08dd167d904d77
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2025-12-20 20:54:36 +0100

    Relocated webdav mount

--- stages/services/includes/nginx/templates/nginx/sites/dav.sh
+++ stages/services/includes/nginx/templates/nginx/sites/dav.sh
@@ -94,7 +94,7 @@ server {
         auth_basic "Protected area";
         auth_basic_user_file /etc/nginx/htpasswd;
         client_max_body_size 0;
-        proxy_pass http://127.0.0.1:38000; 
+        proxy_pass http://10.200.200.5:38000; 
         proxy_set_header Host \$http_host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
