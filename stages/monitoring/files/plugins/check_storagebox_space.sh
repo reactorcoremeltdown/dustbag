f228d7e475f9700c6ae7f50caf3cdbf87b8e948f
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2025-12-29 22:07:27 +0100

    Updated check_storagebox_space plugin

--- stages/monitoring/files/plugins/check_storagebox_space.sh
+++ stages/monitoring/files/plugins/check_storagebox_space.sh
@@ -2,12 +2,12 @@
 
 source /etc/monitoring/plugins/okfail.sh
 
-usage=`ssh -p 23 ${1}@${1}.your-storagebox.de df -h | tail -n 1 | awk '{print $5}' | sed 's|%||'`
+usage=`ssh -p 23 ${OPTION}@${OPTION}.your-storagebox.de df -h | tail -n 1 | awk '{print $5}' | sed 's|%||'`
 
 if [[ $usage -gt ${CRITICAL_THRESHOLD} ]]; then
     fail "The amount of used disk space on $1 is $usage%"
 elif [[ $usage -gt ${WARNING_THRESHOLD} ]]; then
     warning "The amount of used disk space on $1 is $usage%"
 else
-    ok "${usage}% of ${1} is currently used"
+    ok "${usage}% of ${OPTION} is currently used"
 fi
