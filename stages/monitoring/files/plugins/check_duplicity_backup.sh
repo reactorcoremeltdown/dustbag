c98942be3824a7ba998aafd9cc85d76cc37c3cff
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2025-12-29 22:04:52 +0100

    Updated check_duplicity_backup plugin

--- stages/monitoring/files/plugins/check_duplicity_backup.sh
+++ stages/monitoring/files/plugins/check_duplicity_backup.sh
@@ -2,16 +2,16 @@
 
 source /etc/monitoring/plugins/okfail.sh
 
-backup_date=`duplicity collection-status ${1} | grep 'Last full backup date' | cut -f 2- -d ':'`
+backup_date=`duplicity collection-status ${OPTION} | grep 'Last full backup date' | cut -f 2- -d ':'`
 timestamp=`date --date="${backup_date}" +%s`
 current_timestamp=`date +%s`
 
 delta=`echo "(${current_timestamp} - ${timestamp})/86400" | bc`
 
 if test "${delta}" -gt 15; then
-    fail "Last full backup of ${1} was performed ${delta} days ago!"
+    fail "Last full backup of ${OPTION} was performed ${delta} days ago!"
 elif test "${delta}" -gt 6; then
-    warning "Last full backup of ${1} was performed ${delta} days ago"
+    warning "Last full backup of ${OPTION} was performed ${delta} days ago"
 else
-    ok "Last full backup of ${1} was performed ${delta} days ago"
+    ok "Last full backup of ${OPTION} was performed ${delta} days ago"
 fi
