c1dd5c8df9ec0f05bacd45ec8b5de56efcfe8490
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2025-12-29 21:56:38 +0100

    Updated check_website_diff plugin

--- stages/monitoring/files/plugins/check_website_diff.sh
+++ stages/monitoring/files/plugins/check_website_diff.sh
@@ -2,19 +2,19 @@
 
 source /etc/monitoring/plugins/okfail.sh
 
-HASH=$(echo "$1" | md5sum | awk '{print $1}')
+HASH=$(echo "${OPTION}" | md5sum | awk '{print $1}')
 
 OLD_FILE="/tmp/old_file_${HASH}.html"
-test -f ${OLD_FILE} || curl -L -s "$1" > ${OLD_FILE}
+test -f ${OLD_FILE} || curl -L -s "${OPTION}" > ${OLD_FILE}
 
 NEW_FILE="/tmp/new_file_${HASH}.html"
-curl -L -s "$1" > ${NEW_FILE}
+curl -L -s "${OPTION}" > ${NEW_FILE}
 
 diff -q ${OLD_FILE} ${NEW_FILE}
 ret_code=$?
 
 if [[ $ret_code = 0 ]]; then
-    ok "Website $1 unchanged" "$DESCRIPTION"
+    ok "Website ${OPTION} unchanged" "$DESCRIPTION"
 else
-    fail "Website $1 changed!" "$DESCRIPTION"
+    fail "Website ${OPTION} changed!" "$DESCRIPTION"
 fi
