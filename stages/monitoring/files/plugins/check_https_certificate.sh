c7fa2e12ff339098333bcba7d5c9b57787dd35a6
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2025-12-29 21:55:06 +0100

    Updated check_https_certificate plugin

--- stages/monitoring/files/plugins/check_https_certificate.sh
+++ stages/monitoring/files/plugins/check_https_certificate.sh
@@ -2,16 +2,16 @@
 
 source /etc/monitoring/plugins/okfail.sh
 
-pubdate=`echo | openssl s_client -servername $1 -connect $1':443' 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -f 2 -d '='`
+pubdate=`echo | openssl s_client -servername ${OPTION} -connect ${OPTION}':443' 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -f 2 -d '='`
 pubdate_unix=`date +%s -d "$pubdate"`
 current_date=`date +%s`
 let delta="pubdate_unix - current_date"
 let days="delta/86400"
 
 if test $delta -lt 864000; then
-    warning "The SSL cert of $1 will expire in $days days!" "$DESCRIPTION" "$ENVIRONMENT"
+    warning "The SSL cert of ${OPTION} will expire in $days days!" "$DESCRIPTION" "$ENVIRONMENT"
 elif test $delta -lt 259200; then
-    fail "The SSL cert of $1 will expire in $days days!" "$DESCRIPTION" "$ENVIRONMENT"
+    fail "The SSL cert of ${OPTION} will expire in $days days!" "$DESCRIPTION" "$ENVIRONMENT"
 else
     ok "SSL cert is VALID for ${days} days." "$DESCRIPTION" "$ENVIRONMENT"
 fi
