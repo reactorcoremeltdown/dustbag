0f89905f5931aab2f4b1ddb8e323e75ef4211523
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2026-02-19 08:22:21 +0100

    Printing environment variables

--- stages/early/early.mk
+++ stages/early/early.mk
@@ -22,7 +22,7 @@ YQ_DOWNLOAD_URL := "https://github.com/mikefarah/yq/releases/download/v4.45.4/yq
 endif
 
 early_begin:
-	echo $(PWD)
+	printenv
 	iac begin early_stage
 	iac stages/early/files/cfg/basic_files.yaml
 
