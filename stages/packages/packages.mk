2ecd608d4d3bbc5e038b02c838ac187d9c642cc3
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2026-03-04 15:46:25 +0100

    Updated packages

--- stages/packages/packages.mk
+++ stages/packages/packages.mk
@@ -1,9 +1,9 @@
 ifeq ($(MACHINE_ROLE), production)
 	PYTHON_PACKAGES := $(shell yq -r '.python.server[]' stages/packages/variables/packages.yaml | xargs)
-packages: packages_begin packages_generic packages_server packages_end packages_venv
+packages: repos packages_begin packages_generic packages_server packages_end packages_venv
 else
 	PYTHON_PACKAGES := $(shell yq -r '.python.noop[]' stages/packages/variables/packages.yaml | xargs)
-packages: packages_begin packages_generic packages_end packages_venv
+packages: repos packages_begin packages_generic packages_end packages_venv
 endif
 
 packages_begin:
@@ -18,7 +18,7 @@ packages_server:
 packages_generic:
 	iac stages/packages/configs/packages_essentials.yaml
 
-packages_venv: repos
+packages_venv:
 	mkdir -p /opt/virtualenv || true
 	python3 -m venv /opt/virtualenv
 	/opt/virtualenv/bin/pip3 install $(PYTHON_PACKAGES) || true
--- stages/repos/repos.mk
+++ stages/repos/repos.mk
@@ -1,16 +1,13 @@
 ifeq ($(MACHINE_ROLE), production)
-repos: setup_debian_repositories update_debian_repositories reprepro
+repos: setup_debian_repositories reprepro
 	@echo "$(ccgreen)Setting up Debian repositories completed$(ccend)"
 else
-repos: setup_debian_repositories update_debian_repositories
+repos: setup_debian_repositories
 	@echo "$(ccgreen)Setting up Debian repositories completed$(ccend)"
 endif
 
 setup_debian_repositories:
 	bash stages/repos/templates/repo.sh stages/repos/variables/repos.yaml $(MACHINE_ROLE)
-
-update_debian_repositories:
-	apt -y update
 
 reprepro:
 	apt -y install reprepro
