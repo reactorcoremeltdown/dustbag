97f1e65f690cf7340c55d105223972b6be4ceaf2
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2026-03-18 16:20:23 +0100

    Moved ci website nginx config to iacd

--- stages/services/includes/nginx/nginx.mk
+++ stages/services/includes/nginx/nginx.mk
@@ -2,8 +2,8 @@ nginx_proxies:
 	bash stages/services/includes/nginx/templates/nginx/sites/proxies.sh
 
 nginx_sites:
+	iac stages/services/includes/nginx/configs/nginx_sites.yaml
 	bash stages/services/includes/nginx/templates/nginx/sites/bank.sh
-	bash stages/services/includes/nginx/templates/nginx/sites/ci.sh
 	bash stages/services/includes/nginx/templates/nginx/sites/dav.sh
 	bash stages/services/includes/nginx/templates/nginx/sites/default.sh
 	bash stages/services/includes/nginx/templates/nginx/sites/dm.sh
