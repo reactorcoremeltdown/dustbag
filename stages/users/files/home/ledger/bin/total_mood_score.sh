0c27601e26d5d00e615eb8da0907be78a47f19bb
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2026-06-17 10:23:03 +0200

    Logging mood score in achieveburger

--- stages/users/files/home/ledger/bin/total_mood_score.sh
+++ stages/users/files/home/ledger/bin/total_mood_score.sh
@@ -2,4 +2,10 @@
 
 TOTAL_SCORE=$(sqlite3 /home/ledger/expenses.db 'select 50 + (select ifnull(sum(score), 0) from moodscore where time > strftime("%s", date("now", "start of day")));')
 
+if [[ "${TOTAL_SCORE}" -gt 60 ]]; then
+    curl --request POST \
+    --url https://achievements.tiredsysadmin.cc/api/habits/mood/log \
+    --header "Authorization: Bearer $(cat /home/ledger/.achieveburger.txt)"
+fi
+
 sqlite3 /home/ledger/expenses.db "insert into totalmoodscore(score) values(${TOTAL_SCORE})"
