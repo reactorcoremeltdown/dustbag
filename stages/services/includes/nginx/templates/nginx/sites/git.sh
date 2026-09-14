d9d5d3c521865bf20ab8bebe4457fa32f53188cc
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2026-09-14 12:13:14 +0200

    Updated gitea config

--- stages/services/includes/nginx/templates/nginx/sites/git.sh
+++ stages/services/includes/nginx/templates/nginx/sites/git.sh
@@ -51,7 +51,14 @@ server {
     client_max_body_size 200M;
 
     location / {
+        client_max_body_size 512M;
         proxy_pass http://127.0.0.1:25010;
+        proxy_set_header Connection \$http_connection;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
 }
 EOF
