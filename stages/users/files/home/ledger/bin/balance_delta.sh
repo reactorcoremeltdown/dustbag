b5ba42622a41d10c5ef8c0769e8b5c5006964b2b
Reactorcoremeltdown <inbox@reactorcoremeltdown.anonaddy.com>
2026-09-17 15:29:26 +0200

    Query must order by time

--- stages/users/files/home/ledger/bin/balance_delta.sh
+++ stages/users/files/home/ledger/bin/balance_delta.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-BALANCE_DELTA=$(sqlite3 /home/ledger/expenses.db 'select (select balance from balance order by balance desc limit 1 offset 1) - (select balance from balance order by balance desc limit 1)')
+BALANCE_DELTA=$(sqlite3 /home/ledger/expenses.db 'select (select balance from balance order by time desc limit 1 offset 1) - (select balance from balance order by time desc limit 1)')
 
 sqlite3 /home/ledger/expenses.db "insert into balance_delta(delta) values(${BALANCE_DELTA})"
